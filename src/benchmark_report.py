be7bf03075afeec32ada5af117c97f68d1b906cf
Sveinung Rundhovde <ssru@statoil.com>
2019-04-10 15:15:37 +0200

    Add benchmark report generator

---
+++ src/benchmark_report.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import argparse
+import re
+import numpy as np
+
+frgsize_pettern = r'Fragment size: x: (\d*), y: (\d*), z: (\d*)'
+ttl_time_pattern = r'Total elapsed time: (\d*)ms'
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('source_file')
+
+    args = parser.parse_args()
+
+    f = open(args.source_file)
+    results = {}
+
+    while True:
+        line = f.readline()
+
+        if line == '': break
+
+        fragsize = re.search(frgsize_pettern, line).groups()
+        f.readline()
+        f.readline()
+        f.readline()
+        ttl_time = int(re.search(ttl_time_pattern, f.readline()).group(1))
+        f.readline()
+
+        if fragsize in results:
+            results[fragsize].append(ttl_time)
+        else:
+            results[fragsize] = [ttl_time]
+
+    for r in results:
+        a = np.array(results[r])
+        print('{}: avg: {}ms, max: {}ms'.format(r, int(a.mean()), max(a)))
