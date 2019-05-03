83ea5b725bd05ffcf1c5e5b0ddbc8e3e1d3b7f78
Jørgen Kvalsvik <jokva@equinor.com>
2019-05-03 10:36:45 +0200

    Move definitions from include/ to core.cpp
    Add the new shared functionality target semisic-cloud, and move
    implementations from include/seismic-cloud.hpp to src/core.cpp

--- core/include/seismic-cloud/seismic-cloud.hpp
+++ core/include/seismic-cloud/seismic-cloud.hpp
@@ -1,7 +1,7 @@
 #ifndef SEISMIC_CLOUD_HPP
 #define SEISMIC_CLOUD_HPP
 
-#include <cassert>
+#include <vector>
 
 namespace sc {
 
@@ -76,72 +76,29 @@ struct dimension : triple_comparison< dimension > {
         x(x), y(y), z(z) {}
 };
 
-std::ostream& operator << (std::ostream& o, const point& rhs) {
-    return o << "(" << rhs.x << ", " << rhs.y << ", " << rhs.z << ")";
-}
+std::ostream& operator << (std::ostream& o, const point& rhs);
+std::ostream& operator << (std::ostream& o, const dimension& rhs);
 
-std::ostream& operator << (std::ostream& o, const dimension& rhs) {
-    return o << "(" << rhs.x << ", " << rhs.y << ", " << rhs.z << ")";
-}
+point global_to_local(point global, dimension fragment_size) noexcept (true);
+point local_to_global(point local, point root)               noexcept (true);
+point global_to_root(point global, dimension fragment_size)  noexcept (true);
 
-point global_to_local( point global, dimension fragment_size ) {
-    return {
-        global.x % fragment_size.x,
-        global.y % fragment_size.y,
-        global.z % fragment_size.z,
-    };
-}
-
-point local_to_global( point local, point root ) noexcept (true) {
-    return {
-        local.x + root.x,
-        local.y + root.y,
-        local.z + root.z,
-    };
-}
-
-point global_to_root( point global, dimension fragment_size ) {
-    return {
-        (global.x / fragment_size.x) * fragment_size.x,
-        (global.y / fragment_size.y) * fragment_size.y,
-        (global.z / fragment_size.z) * fragment_size.z,
-    };
-}
-
-std::size_t point_to_offset( point p, dimension dim ) {
-    return p.x * dim.y * dim.z
-         + p.y * dim.z
-         + p.z;
-}
-
-point offset_to_point( std::size_t offset, dimension dim ) {
-    return {
-        (offset / (dim.y * dim.z)),
-        (offset % (dim.y * dim.z)) / dim.z,
-        (offset % (dim.y * dim.z)) % dim.z
-    };
-}
-
-std::size_t local_to_global( std::size_t local,
-                             dimension fragment_size,
-                             dimension cube_size,
-                             point root ) {
-
-    auto local_point = offset_to_point( local, fragment_size );
-    auto global = local_to_global( local_point, root );
-
-    return point_to_offset( global, cube_size );
-}
+point offset_to_point(std::size_t offset, dimension dim) noexcept (true);
+std::size_t point_to_offset( point p, dimension dim )    noexcept (true);
+std::size_t local_to_global(std::size_t local,
+                            dimension fragment_size,
+                            dimension cube_size,
+                            point root) noexcept (true);
 
 struct bins {
-    std::vector< sc::point > keys;
+    std::vector< point > keys;
     std::vector< std::size_t > itrs;
     std::vector< std::size_t > data;
 
     using iterator = decltype(data.cbegin());
 
     struct bin {
-        sc::point key;
+        point key;
         iterator first;
         iterator last;
 
@@ -166,56 +123,9 @@ struct bins {
     }
 };
 
-bins bin(sc::dimension fragment_size,
-         sc::dimension cube_size,
-         const std::vector< sc::point >& xs) noexcept (false) {
-
-    using key = std::pair< sc::point, std::size_t >;
-    auto points = std::vector< key >(xs.size());
-
-    auto fragment_id = [fragment_size](const auto& p) noexcept (true) {
-        const auto root  = sc::global_to_root(p, fragment_size);
-        const auto local = sc::global_to_local(p, fragment_size);
-        const auto pos   = sc::point_to_offset(local, fragment_size);
-        return std::make_pair(root, pos);
-    };
-
-    std::transform(xs.begin(), xs.end(), points.begin(), fragment_id);
-    std::sort(points.begin(), points.end());
-
-    /*
-     * If the input surface xs is empty, just return here.  should work and
-     * still just output empty, but accessing vec.front() would be undefined.
-     * Hopefully this check will never be true, because empty requests should
-     * be handled further up in the stack
-     */
-    assert(!xs.empty());
-    bins ret;
-    if (xs.empty()) return ret;
-
-    ret.data.resize(points.size());
-
-    auto snd = [](const auto& x) noexcept (true) { return x.second; };
-    std::transform(points.begin(), points.end(), ret.data.begin(), snd);
-
-    auto prev = points.front().first;
-    std::size_t i = 0;
-    ret.itrs.push_back(i);
-    ret.keys.push_back(prev);
-    for (const auto& p : points) {
-        ++i;
-
-        if (p.first == prev) continue;
-
-        prev = p.first;
-        ret.itrs.push_back(i - 1);
-        ret.keys.push_back(prev);
-    }
-
-    ret.itrs.push_back(points.size());
-
-    return ret;
-}
+bins bin(dimension fragment_size,
+         dimension cube_size,
+         const std::vector< point >& xs) noexcept (false);
 
 }
 
