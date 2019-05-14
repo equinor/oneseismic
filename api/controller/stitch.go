efccd91607368699f17baf8338af353b84723ccf
Sondre Naustdal <sondre.naustdal@gmail.com>
2019-05-14 10:24:22 +0200

    Stitch method fetches manifest

--- api/controller/stitch.go
+++ api/controller/stitch.go
@@ -2,8 +2,11 @@ package controller
 
 import (
 	"bytes"
+	"encoding/binary"
+	"io"
 	"log"
 	"os/exec"
+	"strings"
 
 	"github.com/equinor/seismic-cloud/api/service"
 
@@ -18,12 +21,23 @@ func StitchController(ms service.ManifestStore,
 		manifestID := ctx.Params().Get("manifestID")
 		logger.Printf("Stiching: manifest: %s, surface: %d bytes\n", manifestID, ctx.Request().ContentLength)
 
-		cmd.Stdin = ctx.Request().Body
+		manifest, err := ms.Fetch(manifestID)
+		if err != nil {
+			ctx.StatusCode(500)
+			logger.Println("Stich error:", err)
+			return
+		}
+
+		v := uint32(len(manifest))
+		buf := make([]byte, 4)
+		binary.LittleEndian.PutUint32(buf, v)
+
+		cmd.Stdin = io.MultiReader(strings.NewReader("M:"), bytes.NewBuffer(buf), bytes.NewBuffer(manifest), ctx.Request().Body)
 
 		var buffer bytes.Buffer
 		cmd.Stdout = &buffer
 
-		err := cmd.Run()
+		err = cmd.Run()
 		if err != nil {
 			ctx.StatusCode(500)
 			logger.Println("Stich error:", err)
