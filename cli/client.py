d34d6b56147a85fe4bd2208d7ea8f01658fb585c
Sondre Naustdal <sondre.naustdal@gmail.com>
2019-06-14 12:11:19 +0200

    fix python client

--- cli/client.py
+++ cli/client.py
@@ -1,12 +1,26 @@
+#!/usr/bin/env python3
 import http.client
 import subprocess
 import os
 import argparse
+from urllib.parse import urlparse
 
 
 def getAccessToken() -> str:
     res = subprocess.run(["oauth2local", "token"], stdout=subprocess.PIPE)
+    if res.returncode != 0:
+        raise "No token in ouath2local store " + \
+            res.stdout.decode("utf-8").strip()
     return res.stdout.decode("utf-8").strip()
+
+
+def clientConnector(scUrl):
+    o = urlparse(scUrl)
+    op = o.netloc + o.path
+    if o.scheme == "http":
+        return http.client.HTTPConnection(op)
+    else:
+        return http.client.HTTPSConnection(op)
 
 
 def main():
@@ -16,6 +30,8 @@ def main():
                         help='manifest id for cube')
     parser.add_argument('-i',
                         help='in surface file path')
+    parser.add_argument('-t',
+                        help='access token for user')
     parser.add_argument('-o',
                         help='out surface file path')
     parser.add_argument('--sc-url', dest='scURL',
@@ -31,9 +47,12 @@ def main():
         parser.print_usage()
         return
 
-    at = getAccessToken()
+    if len(args.t) == 0:
+        at = getAccessToken()
+    else:
+        at = args.t
 
-    conn = http.client.HTTPSConnection(args.scURL)
+    conn = clientConnector(args.scUrl)
     conn.request("POST", "/stitch/"+args.manID,
                  headers={"Authorization": "Bearer " + at},
                  body=open(args.i, mode='rb'))
