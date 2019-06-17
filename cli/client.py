afb013fa5f3f07e6738e94a18979f6420d796029
Sondre Naustdal <sondre.naustdal@gmail.com>
2019-06-17 11:51:48 +0200

    Add config file for client

--- cli/client.py
+++ cli/client.py
@@ -4,6 +4,7 @@ import subprocess
 import os
 import argparse
 from urllib.parse import urlparse
+import json
 
 
 def getAccessToken() -> str:
@@ -23,9 +24,50 @@ def clientConnector(scUrl):
         return http.client.HTTPSConnection(op)
 
 
+def sendSurface(scUrl, manID, surface, outFile, at):
+    conn = clientConnector(scUrl)
+    conn.request("POST", "/stitch/"+manID,
+                 headers={"Authorization": "Bearer " + at},
+                 body=open(surface, mode='rb'))
+    r1 = conn.getresponse()
+    if r1.status == 200:
+        if outFile is None:
+            print(r1.read())
+        else:
+            try:
+                b = open(outFile, mode="wb")
+                b.write(r1.read())
+                b.close()
+            except Exception as e:
+                print("Error:" + str(e))
+
+    else:
+        print("Error:" + str(r1.status))
+        print(r1.read())
+
+
+def verify(binFile, cube, surface):
+    return True
+
+
+def sendSurfaces(configFile, at):
+    config = json.loads(open(configFile))
+    outFile = "work.i32"
+    for apiUrl in config.apis:
+        for bench in config.benchs:
+            for surface in bench.surface:
+                open(outFile, 'w').close()
+                sendSurface(apiUrl, bench.cube, surface, outFile, at)
+                if not verify(outFile, bench.cube, surface):
+                    print("Error: Response is not verifiable",
+                          outFile, bench.cube, surface)
+
+
 def main():
     parser = argparse.ArgumentParser(
         description='Send surface to seismic cloud.')
+    parser.add_argument('-c', dest='configFile',
+                        help='Config file with benchmarks')
     parser.add_argument('-m', dest='manID',
                         help='manifest id for cube')
     parser.add_argument('-i',
@@ -52,24 +94,10 @@ def main():
     else:
         at = args.t
 
-    conn = clientConnector(args.scUrl)
-    conn.request("POST", "/stitch/"+args.manID,
-                 headers={"Authorization": "Bearer " + at},
-                 body=open(args.i, mode='rb'))
-    r1 = conn.getresponse()
-    if r1.status == 200:
-        if args.o is None:
-            print(r1.read())
-        else:
-            try:
-                b = open(args.o, mode="wb")
-                b.write(r1.read())
-            except Exception as e:
-                print("Error:" + str(e))
-
+    if args.configFile is None:
+        sendSurface(args.scUrl, args.manID, args.surface, args.outFile, at)
     else:
-        print("Error:" + str(r1.status))
-        print(r1.read())
+        sendSurfaces(args.configFile, at)
 
 
 if __name__ == '__main__':
