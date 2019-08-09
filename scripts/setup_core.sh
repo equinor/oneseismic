06900f83288a148c9b9802c46cb7875e079833f2
Kristian Reed <krr@equinor.com>
2019-08-09 15:26:12 +0200

    Update docker and git ignore files

--- scripts/setup_core.sh
+++ scripts/setup_core.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Build seismic-cloud core and create sample cubes and surfaces
+# Execute from seismic-cloud root folder
 
 rm -rf core/build
 rm -rf tmp/testfiles
@@ -11,4 +12,4 @@ cmake .. -DCMAKE_BUILD_TYPE=Release
 make  -j4
 popd
 ./core/build/generate_surfaces tmp/testfiles/surfaces/
-./core/build/generate 1000 1000 1000 100 100 100 -o tmp/testfiles/cubes/
+./core/build/generate 4000 1000 1000 100 100 100 -o tmp/testfiles/cubes/
