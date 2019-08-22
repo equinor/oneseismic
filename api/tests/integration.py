1791c5aabcc0582a01dfad289cd50eafb7fca11c
Kristian Reed <krr@equinor.com>
2019-08-22 14:04:35 +0200

    fix

--- api/tests/integration.py
+++ api/tests/integration.py
@@ -7,15 +7,16 @@ import requests
 import subprocess
 
 subprocess.call(["../api", "defaults", "--config", ".sc-api.yaml"])
-os.environ["STITCH_CMD"] = "/usr/bin/cat"
+os.environ["STITCH_CMD"] = "ls"
 os.environ["MANIFEST_SRC"] = "path"
-os.environ["MANIFEST_PATH"] = "./"
+# os.environ["MANIFEST_PATH"] = ""
 os.environ["NO_AUTH"] = "True"
 os.environ["HTTP_ONLY"] = "True"
 os.environ["HOST_ADDR"] = "localhost:8070"
 
 f = open("sample.manifest", "w")
-f.write("my2_manifest")
+f.write("my_manifest\n")
+f.close()
 
 
 def test_get_main_page():
@@ -32,7 +33,7 @@ def test_post():
     r = requests.post('http://localhost:8070/stitch/sample',
                       data={'please': 'return'})
     subprocess.Popen.kill(p)
-    assert r.content == b'M:\t\x00\x00\x00my_manifest\nplease=return'
+    assert r.content == b'M:\x0c\x00\x00\x00my_manifest\nplease=return'
     subprocess.Popen.kill(p)
 
 if __name__ == "__main__":
