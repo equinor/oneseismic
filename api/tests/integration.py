d7637da98fcc12a73de649c3275e8fbf64b96de5
Kristian Reed <krr@equinor.com>
2019-08-22 20:08:30 +0200

    defaults test

--- api/tests/integration.py
+++ api/tests/integration.py
@@ -3,11 +3,9 @@
 import os
 import pytest
 import requests
-
 import subprocess
 from time import sleep
 
-subprocess.call(["../api", "defaults", "--config", ".sc-api.yaml"])
 os.environ["STITCH_CMD"] = "/bin/cat"
 os.environ["MANIFEST_SRC"] = "path"
 os.environ["MANIFEST_PATH"] = "./"
@@ -15,28 +13,33 @@ os.environ["NO_AUTH"] = "True"
 os.environ["HTTP_ONLY"] = "True"
 os.environ["HOST_ADDR"] = "localhost:7020"
 
-f = open("sample.manifest", "w")
-f.write("my_manifest\n")
-f.close()
+
+def test_create_defaults():
+    subprocess.call(["../api", "defaults", "--config", ".sc-api.yaml"])
+    assert os.path.exists('.sc-api.yml') == True
 
 
 def test_get_main_page():
     p = subprocess.Popen(
         ["../api serve --config .sc-api.yaml"], shell=True)
-    sleep(2)
+    sleep(1)
     r = requests.get('http://localhost:7020/')
     p.kill()
     assert r.status_code == 200
 
+
 def test_post():
+    f = open("sample.manifest", "w")
+    f.write("my_manifest\n")
+    f.close()
+
     p = subprocess.Popen(
         ["../api serve --config .sc-api.yaml"], shell=True)
     r = requests.post('http://localhost:7020/stitch/sample',
                       data={'please': 'return'})
-    sleep(2)
+    sleep(1)
     p.kill()
     assert r.content == b'M:\x0c\x00\x00\x00my_manifest\nplease=return'
-    subprocess.Popen.kill(p)
 
 
 if __name__ == "__main__":
