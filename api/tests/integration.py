33d101a588c3fbb70da0e86a6ce280e8ff34a10b
Kristian Reed <krr@equinor.com>
2019-08-22 19:05:29 +0200

    fix shell command

--- api/tests/integration.py
+++ api/tests/integration.py
@@ -5,14 +5,15 @@ import pytest
 import requests
 
 import subprocess
+from time import sleep
 
 subprocess.call(["../api", "defaults", "--config", ".sc-api.yaml"])
-os.environ["STITCH_CMD"] = "/usr/bin/cat"
+os.environ["STITCH_CMD"] = "/bin/cat"
 os.environ["MANIFEST_SRC"] = "path"
-# os.environ["MANIFEST_PATH"] = ""
+os.environ["MANIFEST_PATH"] = "./"
 os.environ["NO_AUTH"] = "True"
 os.environ["HTTP_ONLY"] = "True"
-os.environ["HOST_ADDR"] = "localhost:8070"
+os.environ["HOST_ADDR"] = "localhost:7020"
 
 f = open("sample.manifest", "w")
 f.write("my_manifest\n")
@@ -21,20 +22,22 @@ f.close()
 
 def test_get_main_page():
     p = subprocess.Popen(
-        ["../api", "serve", "--config", ".sc-api.yaml"])
-    r = requests.get('http://localhost:8070/')
-    subprocess.Popen.kill(p)
+        ["../api serve --config .sc-api.yaml"], shell=True)
+    sleep(2)
+    r = requests.get('http://localhost:7020/')
+    p.kill()
     assert r.status_code == 200
-
 
 def test_post():
     p = subprocess.Popen(
-        ["../api", "serve", "--config", ".sc-api.yaml"])
-    r = requests.post('http://localhost:8070/stitch/sample',
+        ["../api serve --config .sc-api.yaml"], shell=True)
+    r = requests.post('http://localhost:7020/stitch/sample',
                       data={'please': 'return'})
-    subprocess.Popen.kill(p)
+    sleep(2)
+    p.kill()
     assert r.content == b'M:\x0c\x00\x00\x00my_manifest\nplease=return'
     subprocess.Popen.kill(p)
+
 
 if __name__ == "__main__":
     test_get_main_page()
