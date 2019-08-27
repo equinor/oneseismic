3d2bb2fa4231643ba0d1675b926fc9ee38e10364
Sondre Naustdal <sondre.naustdal@gmail.com>
2019-08-27 12:40:56 +0200

    Build core in container for ubuntu

--- core/core-server/server/server.py
+++ core/core-server/server/server.py
@@ -11,6 +11,7 @@ from .proto import core_pb2_grpc
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
+
 class Core(core_pb2_grpc.CoreServicer):
 
     def __init__(self, base_dir):
@@ -31,7 +32,7 @@ class Core(core_pb2_grpc.CoreServicer):
 def serve(base_dir):
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     core_pb2_grpc.add_CoreServicer_to_server(Core(base_dir), server)
-    server.add_insecure_port('localhost:5005')
+    server.add_insecure_port('0.0.0.0:5005')
     server.start()
     try:
         while True:
@@ -40,10 +41,13 @@ def serve(base_dir):
         server.stop(0)
 
 
-
 def main():
+    if sys.argv[1] == "help":
+        print("Core grpc server v0.1b")
+        exit(0)
     logging.basicConfig()
     serve(sys.argv[1])
+
 
 if __name__ == '__main__':
     main()
--- core/entrypoint.sh
+++ core/entrypoint.sh
@@ -4,4 +4,13 @@ set -e
 echo "yoyou"
 echo "Running nc on  $@"
 
-nc -lk -p 5000 -e "$@"
+# mount -t tmpfs -o size=16g tmpfs /mnt/ramdisk
+# mkdir /mnt/ramdisk/blobfusetmp
+# chown appuser /mnt/ramdisk/blobfusetmp
+
+
+touch ~/fuse_connection.cfg
+chmod 600 ~/fuse_connection.cfg
+~/fuse_connection.cfg < envsubst < tmp_fuse_connection.cfg
+# nc -lk -p 5000 -e "$@"
+exec "$@"
