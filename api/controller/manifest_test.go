c65143bcc5ea513087d0a7a0043f48e73106b802
Eivind Jahren <eivind.jahren@webstep.no>
2019-10-21 14:55:29 +0200

    fixup! Add tests for manifests

--- api/controller/manifest_test.go
+++ api/controller/manifest_test.go
@@ -4,17 +4,13 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http/httptest"
-	"os"
 	"testing"
 
-	"github.com/equinor/seismic-cloud/api/events"
-	l "github.com/equinor/seismic-cloud/api/logger"
 	"github.com/equinor/seismic-cloud/api/service/store"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestManifestControllerList(t *testing.T) {
-	l.SetLogSink(os.Stdout, events.DebugLevel)
 	manifest := NewTestManifest()
 	ts := NewTestSetup()
 	ts.AddManifest("12345", manifest)
@@ -34,5 +30,42 @@ func TestManifestControllerList(t *testing.T) {
 	err = json.Unmarshal(gotManifests, &readManifests)
 	assert.Nil(t, err)
 
-	assert.Equal(t, readManifests[0], manifest)
+	assert.Equal(t, readManifests, []store.Manifest{manifest})
+}
+
+func TestManifestControllerFetch(t *testing.T) {
+	manifest := NewTestManifest()
+	ts := NewTestSetup()
+	ts.AddManifest("12345", manifest)
+
+	req := httptest.NewRequest("GET", "/manifest/12345", nil)
+	ts.BeginRequest(req)
+    ts.SetParam("manifestID", "12345")
+
+	ts.ManifestController.Fetch(ts.Ctx)
+	assert.Equal(t, ts.Ctx.GetStatusCode(), 200)
+
+	ts.EndRequest()
+
+	gotManifests, err := ioutil.ReadAll(ts.Result().Body)
+	assert.Nil(t, err)
+
+	readManifests := store.Manifest{}
+	err = json.Unmarshal(gotManifests, &readManifests)
+	assert.Nil(t, err)
+
+	assert.Equal(t, readManifests, manifest)
+}
+
+func TestManifestControllerFetchMissing(t *testing.T) {
+	ts := NewTestSetup()
+
+	req := httptest.NewRequest("GET", "/manifest/12345", nil)
+	ts.BeginRequest(req)
+    ts.SetParam("manifestID", "12345")
+
+	ts.ManifestController.Fetch(ts.Ctx)
+	assert.Equal(t, ts.Ctx.GetStatusCode(), 404)
+
+	ts.EndRequest()
 }
