e801b6574b6839e51087c2c00908cfcb9156b86b
Kjell Kongsvik <kjell.kongsvik@gmail.com>
2019-10-23 13:49:06 +0200

    generate manifest in test

--- tests/integration_tests.py
+++ tests/integration_tests.py
@@ -5,8 +5,10 @@ import pytest
 import requests
 from struct import pack, unpack
 import math
+import json
 
 uri = os.environ.get("SC_API_HOST_ADDR", "http://localhost:8080")
+fixtures = os.environ.get("MANIFEST_PATH", "/fixtures")
 
 
 def test_happy():
@@ -19,10 +21,19 @@ def test_sad():
 
 
 def test_get_post():
-
+    manifest = {
+        "basename":"testmanifest",
+        "cubexs":1,
+        "cubeys":1,
+        "cubezs":1,
+        "fragmentxs":1,
+        "fragmentys":1,
+        "fragmentzs":1
+    }
     r = requests.get(uri)
     assert r.status_code == 200
-
+    with open(fixtures+"/sample", "+w") as f:
+        json.dump(manifest, f)
     data = (1,2,3)
     r = requests.post(uri + "/stitch/sample", data=pack("<fff", *data))
     got = unpack("<qf", r.content)
