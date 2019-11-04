05cf2306103e9ea0b8bb3f96548fad458b7c27da
Jørgen Kvalsvik <jokva@equinor.com>
2019-11-04 14:53:15 +0100

    Divorce scan/segmenter from openvds
    Break away from producing openvds-SEGYScan compatible files, both in
    keys and content. This is driven by considerations:
    
    1. The openvds names are quite clunky
    2. The contents are not actually openvds compatible, so it's a good
       thing to not have them easily confused
    
    The major content changes is guid being sha256 based, instead of a
    timestamp (which, granted, was always the case for this implementation).
    Other than that, ranges are no longer closed intervals with begin/end
    line numbers - instead, we preserve the full in/crossline table for all
    dimensions.
    
    In this iteration, only the three dimensions inline/crossline/samples
    are supported, but the format itself is easily generalisable to N
    dimensions. In fact, the dimensions themselves are not actually named,
    rather indexed in order.

--- core/ingestion/scan/scan/segmenter.py
+++ core/ingestion/scan/scan/segmenter.py
@@ -8,8 +8,9 @@ class segmenter:
     def __init__(self, primary, secondary, endian):
         self.primary = primary
         self.secondary = secondary
-        self.segments = []
-        self.current = None
+        self.primaries = []
+        self.secondaries = []
+        self.previous_secondary = None
         self.traceno = 0
         self.intp = parseint(endian = endian, default_length = 4)
 
@@ -40,77 +41,53 @@ class segmenter:
         ...     seg.add(header)
         >>> seg.commit()
         """
+
         key1 = self.intp.parse(header[self.primary])
         key2 = self.intp.parse(header[self.secondary])
-        cdpx = float(self.intp.parse(header[segyio.su.cdpx]))
-        cdpy = float(self.intp.parse(header[segyio.su.cdpy]))
 
-        if self.current is not None and key1 != self.current['primaryKey']:
-            self.commit()
+        def increment():
+            self.traceno += 1
+            self.previous_secondary = key2
 
-        if self.current is None:
-            logging.debug('starting new segment from trace %', self.traceno)
-            self.current = {
-                'primaryKey': key1,
-                'traceStart': self.traceno,
-                'binInfoStart': {
-                    'inlineNumber': key1,
-                    'crosslineNumber': key2,
-                    'ensembleXCoordinate': cdpx,
-                    'ensembleYCoordinate': cdpy,
-                },
-                'binInfoStop': {
-                    'inlineNumber': key1,
-                    'crosslineNumber': key2,
-                    'ensembleXCoordinate': cdpx,
-                    'ensembleYCoordinate': cdpy,
-                },
-            }
-
-        stop = self.current['binInfoStop']
-        stop['inlineNumber'] = key1
-        stop['crosslineNumber'] = key2
-        self.traceno += 1
-
-    def commit(self):
-        """Commit the current segment
-
-        Close and commit the current segment, and add it to the list of
-        segments. This method *must* be called when the file is exhausted, to
-        properly close the last segment.
-
-        Warns
-        -----
-        UserWarning
-            If commit is called without a current object, usually from a
-            missing add()
-
-        Notes
-        -----
-        add is aware of this function, and will commit and close all other
-        segments than the last automatically from the headers.
-
-        Examples
-        --------
-        Typical use:
-        >>> seg = segmenter(...)
-        >>> for header in headers:
-        ...     seg.add(header)
-        >>> seg.commit()
-        """
-
-        if self.current is None:
-            msg = 'commit called without current object, did you forget add()?'
-            warnings.warn(msg)
+        # This is the first trace
+        if not self.primaries and not self.secondaries:
+            self.primaries = [key1]
+            self.secondaries = [key2]
+            increment()
             return
 
-        logging.debug('committing segment [%:%]',
-            self.current['traceStart'],
-            self.traceno,
-        )
-        # traceno has already been incremented, because a segment is closed and
-        # committed *after* the next segment has been added, and scan is
-        # expected to produce closed intervals
-        self.current['traceStop'] = self.traceno - 1
-        self.segments.append(self.current)
-        self.current = None
+        # The secondary wraps
+        if key2 == self.secondaries[0]:
+
+            if key1 in self.primaries:
+                raise RuntimeError("The file ...")
+
+            if self.previous_secondary != self.secondaries[-1]:
+                raise RuntimeError("The file ...")
+
+            self.primaries.append(key1)
+            increment()
+            return
+
+        # We are encountering a new secondary
+        if self.previous_secondary == self.secondaries[-1]:
+
+            if key2 in self.secondaries:
+                raise RuntimeError("The file ...")
+
+            if key1 != self.primaries[-1]:
+                raise RuntimeError("The file ...")
+
+            if len(self.primaries) > 1:
+                raise RuntimeError("The file ...")
+
+            self.secondaries.append(key2)
+            increment()
+            return
+
+        # By now we know that we are encountering a known secondary that does
+        # not wrap. Check that it follows the pattern of previous segments
+        key2_index = self.secondaries.index(key2)
+        if  self.secondaries[key2_index - 1] != self.previous_secondary:
+            raise RuntimeError("The file ...")
+        increment()
