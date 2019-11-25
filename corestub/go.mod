6f1224b8bc37e50626afaed527da3d361265e4c8
Christian de Jonge <chjo@equinor.com>
2019-11-25 12:00:35 +0100

    Remove duplicate code from corestub and made corestub into an importable package Add token to gitconfig for private repos

--- corestub/go.mod
+++ corestub/go.mod
@@ -3,16 +3,6 @@ module github.com/equinor/seismic-cloud-api/corestub
 go 1.13
 
 require (
-	github.com/Azure/azure-pipeline-go v0.2.2 // indirect
-	github.com/Azure/azure-storage-blob-go v0.8.0
-	github.com/Azure/go-autorest/autorest v0.9.1 // indirect
-	github.com/Azure/go-autorest/autorest/adal v0.6.0 // indirect
-	github.com/golang/protobuf v1.3.2
-	github.com/mattn/go-ieproxy v0.0.0-20191113090002-7c0f6868bffe // indirect
-	github.com/pkg/errors v0.8.1 // indirect
-	golang.org/x/net v0.0.0-20191119073136-fc4aabc6c914 // indirect
-	golang.org/x/sys v0.0.0-20191120155948-bd437916bb0e // indirect
-	google.golang.org/genproto v0.0.0-20191115221424-83cc0476cb11 // indirect
+	github.com/equinor/seismic-cloud-api/api v0.0.0-20191122152025-a6eac62ac5f6
 	google.golang.org/grpc v1.25.1
-	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
---
+++ corestub/server/server.go
@@ -0,0 +1,202 @@
+package server
+
+import (
+	"context"
+	"encoding/binary"
+	"io"
+	"os"
+
+	"fmt"
+	"log"
+	"math"
+	"net"
+
+	pb "github.com/equinor/seismic-cloud-api/api/proto"
+	store "github.com/equinor/seismic-cloud-api/api/service/store"
+	"google.golang.org/grpc"
+)
+
+type coreServer struct {
+	storeAddr string
+}
+
+type Surface struct {
+	Points []*Point
+}
+
+type Point struct {
+	X uint64
+	Y uint64
+	Z uint64
+}
+
+var ss store.SurfaceStore
+
+func wrapSurfaceReader(in io.Reader) (Surface, error) {
+	var surf Surface
+	surf, err := decodeSurface(in)
+	if err != nil {
+		return surf, fmt.Errorf("downloading surface: %w", err)
+	}
+	return surf, nil
+}
+
+func decodeSurface(in io.Reader) (Surface, error) {
+	var surface Surface
+	for {
+		var p struct {
+			X, Y, Z uint64
+		}
+		err := binary.Read(in, binary.LittleEndian, &p)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return surface, fmt.Errorf("decoding surface: %w", err)
+		}
+		surface.Points = append(surface.Points, &Point{X: p.X, Y: p.Y, Z: p.Z})
+	}
+	return surface, nil
+}
+
+func bound(x, y, z uint64) func(p *Point) bool {
+
+	return func(p *Point) bool {
+		if p.X < 0 || p.Y < 0 || p.Z < 0 {
+			return false
+		}
+		if p.X > x || p.Y > y || p.Z > z {
+			return false
+		}
+		return true
+	}
+}
+
+func genFunc(g string, x, y, z uint32) func(p *Point) float32 {
+
+	b := bound(uint64(x), uint64(y), uint64(z))
+	switch g {
+	case "checker":
+		return checkerCube(b)
+	case "gradient":
+		return gradientCube(b)
+	case "sin":
+		return sinCube(b)
+	default:
+		return sinCube(b)
+	}
+}
+
+func checkerCube(b func(p *Point) bool) func(p *Point) float32 {
+
+	return func(p *Point) float32 {
+		if !b(p) {
+			return 0.0
+		}
+		x, y, z := p.X, p.Y, p.Z
+
+		mx, my, mz := x%2 == 0, y%2 == 0, z%2 == 0
+
+		if mx != my {
+			if mz {
+				return 0.0
+			}
+			return 1.0
+
+		}
+
+		if mz {
+			return 1.0
+		}
+		return 0.0
+
+	}
+}
+
+func gradientCube(b func(p *Point) bool) func(p *Point) float32 {
+	return func(p *Point) float32 {
+		if !b(p) {
+			return 0
+		}
+		return float32(p.Z)
+	}
+}
+
+func sinCube(b func(p *Point) bool) func(p *Point) float32 {
+	return func(p *Point) float32 {
+		if !b(p) {
+			return 0.0
+		}
+		return float32(math.Sin(float64(p.Z)))
+	}
+}
+
+func (s *coreServer) StitchSurface(ctx context.Context, in *pb.SurfaceRequest) (*pb.SurfaceReply, error) {
+
+	fmt.Println("stitching on cube ", in.Basename)
+	vals := []*pb.SurfaceReplyValue{}
+	cube := genFunc(in.Basename, in.Cubexs, in.Cubeys, in.Cubezs)
+	id := in.Surfaceid
+	r, err := ss.Download(ctx, id)
+	if err != nil {
+		fmt.Println("surface download failed")
+		return nil, err
+	}
+
+	surf, err := wrapSurfaceReader(r)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("size of surface is ", len(surf.Points))
+	for idx, p := range surf.Points {
+		val := &pb.SurfaceReplyValue{I: uint64(idx), V: cube(p)}
+		vals = append(vals, val)
+	}
+	fmt.Println("size of repl is ", len(vals))
+	repl := &pb.SurfaceReply{SurfaceReplyValues: vals}
+	return repl, nil
+}
+
+func (s *coreServer) ShatterLink(ctx context.Context, in *pb.ShatterLinkRequest) (*pb.ShatterReply, error) {
+	return nil, nil
+}
+
+func StartServer(hostAddr string) {
+
+	var err error
+	ss, err = store.NewSurfaceStore(surfaceStoreConfig())
+	if err != nil {
+		panic(fmt.Errorf("No surface store, error: %v", err))
+	}
+
+	cs := &coreServer{storeAddr: ""}
+
+	fmt.Println("starting server on ", hostAddr)
+	lis, err := net.Listen("tcp", hostAddr)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterCoreServer(grpcServer, cs)
+	grpcServer.Serve(lis)
+}
+
+func surfaceStoreConfig() interface{} {
+
+	if len(os.Getenv("AZURE_STORAGE_ACCOUNT")) > 0 && len(os.Getenv("AZURE_STORAGE_ACCESS_KEY")) > 0 {
+		return store.AzureBlobSettings{
+			AccountName:   os.Getenv("AZURE_STORAGE_ACCOUNT"),
+			AccountKey:    os.Getenv("AZURE_STORAGE_ACCESS_KEY"),
+			ContainerName: "scblob",
+		}
+	}
+
+	if len(os.Getenv("LOCAL_SURFACE_PATH")) > 0 {
+		return store.BasePath(os.Getenv("LOCAL_SURFACE_PATH"))
+	}
+
+	return store.BasePath("/tmp/")
+
+}
