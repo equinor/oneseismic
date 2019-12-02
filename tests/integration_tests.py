ad70fb51f31de84ed021967699fe4eadc7b9a08e
Eivind Jahren <eivind.jahren@webstep.no>
2019-12-02 16:29:13 +0100

    Use auth in integration tests (#38)

--- tests/integration_tests.py
+++ tests/integration_tests.py
@@ -1,12 +1,13 @@
 import os
 import subprocess
+from struct import pack, unpack
+from urllib.parse import parse_qs, urlparse
 
 import pytest
 import requests
-from struct import pack, unpack
 
-
-uri = os.environ.get("SC_API_HOST_ADDR", "http://localhost:8080")
+sc_uri = os.environ.get("SC_API_HOST_ADDR", "http://localhost:8080")
+auth_uri = os.environ.get("AUTH_ADDR", "http://localhost:8089")
 fixturesPath = os.environ.get("FIXTURES_PATH")
 
 manifest_data = b'{"basename":"checker","cubexs":0,"cubeys":0,"cubezs":0,"fragmentxs":0,"fragmentys":0,"fragmentzs":0}'
@@ -21,52 +22,65 @@ with open(fixturesPath + "/surfaces/test-surface", "wb") as f:
     f.write(surface_data)
 
 
+r = requests.get(auth_uri + "/oauth2/authorize", allow_redirects=False)
+assert r.status_code == 302
+
+token = parse_qs(urlparse(r.headers["Location"]).fragment)["access_token"][0]
+
+auth_header = {"Authorization": f"bearer {token}"}
+
+
+def test_version_no_auth():
+    r = requests.get(sc_uri)
+    assert r.status_code == 401
+
+
 def test_version():
-    r = requests.get(uri)
+    r = requests.get(sc_uri, headers=auth_header)
     assert r.status_code == 200
     assert r.content.startswith(b"Seismic Cloud API ")
 
 
 def test_surface_list():
-    r = requests.get(uri + "/surface")
+    r = requests.get(sc_uri + "/surface", headers=auth_header)
     assert r.status_code == 200
 
 
 def test_manifest_list():
-    r = requests.get(uri + "/manifest")
+    r = requests.get(sc_uri + "/manifest", headers=auth_header)
     assert r.status_code == 200
 
 
 def test_surface_get():
-    r = requests.get(uri + "/surface/test-surface")
+    r = requests.get(sc_uri + "/surface/test-surface", headers=auth_header)
     assert r.status_code == 200
     assert r.content == surface_data
 
 
 def test_surface_get_fail():
-    r = requests.get(uri + "/surface/not-exist")
+    r = requests.get(sc_uri + "/surface/not-exist", headers=auth_header)
     assert r.status_code == 404
 
 
 def test_manifest_get():
-    r = requests.get(uri + "/manifest/test-manifest")
+    r = requests.get(sc_uri + "/manifest/test-manifest", headers=auth_header)
     assert r.status_code == 200
     assert r.content == manifest_data
 
 
 def test_stitch():
-    r = requests.get(uri + "/stitch/test-manifest/test-surface")
+    r = requests.get(sc_uri + "/stitch/test-manifest/test-surface", headers=auth_header)
     assert r.status_code == 200
     assert r.content == stitch_response
 
 
 def test_stitch_fail_manifest():
-    r = requests.get(uri + "/stitch/no-exist/test-surface")
+    r = requests.get(sc_uri + "/stitch/no-exist/test-surface", headers=auth_header)
     assert r.status_code == 404
     assert r.content == b"Not Found"
 
 
 def test_stitch_fail_surface():
-    r = requests.get(uri + "/stitch/test-manifest/no-surface")
+    r = requests.get(sc_uri + "/stitch/test-manifest/no-surface", headers=auth_header)
     assert r.status_code == 500
     assert r.content == b"Internal Server Error"
