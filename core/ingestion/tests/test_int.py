fc7e2f150e7ca1d5a7a72971de3a02f475488986
Kjell Kongsvik <kjell.kongsvik@gmail.com>
2020-01-08 16:03:17 +0100

    Initial BlobIO

--- core/ingestion/tests/test_int.py
+++ core/ingestion/tests/test_int.py
@@ -3,6 +3,7 @@ import os
 import pytest
 from azure.storage.blob import BlobServiceClient
 
+from blobio import BlobIO
 from upload import upload
 
 host = os.getenv("AZURITE", "localhost")
@@ -13,6 +14,12 @@ BlobEndpoint=http://{}:10000/devstoreaccount1;""".format(
     host
 )
 
+
+container = "dir"
+filename = "file"
+data = b"data"
+
+
 @pytest.fixture(scope="session")
 def delete_all_containers():
     blob = BlobServiceClient.from_connection_string(connect_str)
@@ -20,13 +27,26 @@ def delete_all_containers():
         blob.delete_container(c)
 
 
+@pytest.fixture
+def blob_service_client(scope="session"):
+    bsc = BlobServiceClient.from_connection_string(connect_str)
+    bsc.create_container(container)
+    blob_client = bsc.get_blob_client(container=container, blob=filename)
+    blob_client.upload_blob(data)
+    yield bsc
+    bsc.delete_container(container)
+
+
+def test_blobio(blob_service_client, delete_all_containers):
+
+    stream = BlobIO(blob=blob_service_client, container=container, filename=filename,)
+
+    assert stream.read() == data
+
+
 def test_upload(delete_all_containers):
     params = {
-        'subcube-dims': (
-            120,
-            120,
-            120,
-        ),
+        "subcube-dims": (120, 120, 120,),
     }
 
     blob = BlobServiceClient.from_connection_string(connect_str)
