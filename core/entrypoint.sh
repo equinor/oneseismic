ce6927cac2dcbb68d26d63651b23978ba83f6e3d
Sondre Naustdal <sondre.naustdal@gmail.com>
2020-01-29 10:23:46 +0100

    fix fmt

--- core/entrypoint.sh
+++ core/entrypoint.sh
@@ -5,4 +5,4 @@ echo "Running oneseismic-core"
 
 echo " on -A $AZ_BLOB_ACCOUNT"
 
-./one-server -A "$AZ_BLOB_ACCOUNT" -k "$AZ_BLOB_KEY"
+./one-server -b "0.0.0.0:50051" -A "$AZ_BLOB_ACCOUNT" -k "$AZ_BLOB_KEY"
