85e13d0b6e4a481923549bd22798a7441191d758
Kjell Kongsvik <kjell.kongsvik@gmail.com>
2020-02-12 09:35:31 +0100

    API: Version default blank
    Version needs to be explicitly set during build.
    If missing it will not try to call a git process.

--- api/main.go
+++ api/main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
-	"os/exec"
 	"runtime"
 
 	"github.com/equinor/seismic-cloud/api/cmd"
@@ -14,38 +12,9 @@ import (
 )
 
 var AppName string = "sc-api"
-var Version string = "v0.0.0"
-
-func getVersionFromGit() string {
-
-	versionCmd := exec.Command("git", "describe", "--always", "--long", "--dirty")
-	branchCmd := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD")
-	run := func(cmd *exec.Cmd) string {
-		stdout, err := cmd.StdoutPipe()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if err := cmd.Start(); err != nil {
-			log.Fatal(err)
-		}
-
-		ret, err := ioutil.ReadAll(stdout)
-		if err != nil {
-			log.Fatal(err)
-		}
-		return string(ret[:len(ret)-1])
-	}
-	branch := run(branchCmd)
-	version := run(versionCmd)
-	v := fmt.Sprintf("%s-%s", branch, version)
-	return v
-}
+var Version string
 
 func init() {
-	if Version == "v0.0.0" {
-		Version = getVersionFromGit()
-	}
 	Version += " " + runtime.Version()
 	config.SetVersion(Version)
 
