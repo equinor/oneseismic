94a9d09eca762872330fe6b0e503aabf5d308841
Jørgen Kvalsvik <jokva@equinor.com>
2020-03-04 10:25:55 +0100

    Remove the sc namespaced geometry module
    The geometry module was moved as-is to the one namespace, and this
    commit finally removes the sc namespaced implementation.

--- core/tests/geometry.cpp
+++ core/tests/geometry.cpp
@@ -1,6 +1,6 @@
 #include <catch/catch.hpp>
 
-#include <seismic-cloud/geometry.hpp>
+#include <oneseismic/geometry.hpp>
 #include "generators.hpp"
 
 using namespace Catch::Matchers;
@@ -9,18 +9,18 @@ SCENARIO( "Converting between global and local coordinates" ) {
 
     GIVEN("A point in global grid is divisible "
           "by the subcube dimensions") {
-        sc::CP< 3 > p {100, 200, 110};
-        sc::CS< 3 > cube_size {2000, 2000, 1000};
-        sc::FS< 3 > frag_size {20, 20, 10};
+        one::CP< 3 > p {100, 200, 110};
+        one::CS< 3 > cube_size {2000, 2000, 1000};
+        one::FS< 3 > frag_size {20, 20, 10};
 
-        const auto co = sc::gvt< 3 >(cube_size, frag_size);
+        const auto co = one::gvt< 3 >(cube_size, frag_size);
 
         WHEN("Converting to local coordinates") {
             const auto local = co.to_local(p);
 
             THEN("The point should be in origo in "
                  "the local coordinate system") {
-                CHECK(local == sc::FP< 3 > {0, 0, 0});
+                CHECK(local == one::FP< 3 > {0, 0, 0});
             }
 
             THEN("The point can be converted back to global coordinates") {
@@ -33,17 +33,17 @@ SCENARIO( "Converting between global and local coordinates" ) {
 
     GIVEN( "A point in global grid not divisible "
            "by the fragment dimension< 3 >s" ) {
-        sc::CP< 3 > p {55, 67, 88};
-        sc::CS< 3 > cube {220, 200, 100};
-        sc::FS< 3 > frag {22, 20, 10};
+        one::CP< 3 > p {55, 67, 88};
+        one::CS< 3 > cube {220, 200, 100};
+        one::FS< 3 > frag {22, 20, 10};
 
-        const auto co = sc::gvt< 3 >(cube, frag);
+        const auto co = one::gvt< 3 >(cube, frag);
 
         WHEN("Converting to local coordinates") {
             const auto local = co.to_local(p);
 
             THEN("The point is correctly converted to local coordiantes") {
-                CHECK(local == sc::FP< 3 > {11, 7, 8});
+                CHECK(local == one::FP< 3 > {11, 7, 8});
             }
 
             THEN("The point can be converted back to global coordiantes") {
@@ -55,24 +55,24 @@ SCENARIO( "Converting between global and local coordinates" ) {
     }
 
     GIVEN("Points that should be mapped to the fragment (upper) corners") {
-        const sc::CP< 3 > p1 {98, 59, 54};
-        const sc::CP< 3 > p2 {65, 79, 109};
+        const one::CP< 3 > p1 {98, 59, 54};
+        const one::CP< 3 > p2 {65, 79, 109};
 
-        const sc::FS< 3 > frag1 {33, 20, 11};
-        const sc::FS< 3 > frag2 {22, 20, 10};
+        const one::FS< 3 > frag1 {33, 20, 11};
+        const one::FS< 3 > frag2 {22, 20, 10};
 
-        const sc::CS< 3 > cube {220, 200, 1000};
+        const one::CS< 3 > cube {220, 200, 1000};
 
-        const auto co1 = sc::gvt< 3 >(cube, frag1);
-        const auto co2 = sc::gvt< 3 >(cube, frag2);
+        const auto co1 = one::gvt< 3 >(cube, frag1);
+        const auto co2 = one::gvt< 3 >(cube, frag2);
 
         WHEN("Converting to local coordinates") {
             const auto local1 = co1.to_local(p1);
             const auto local2 = co2.to_local(p2);
 
             THEN("The point is mapped to the subcubes (upper) corner") {
-                CHECK(local1 == sc::FP< 3 > {32, 19, 10});
-                CHECK(local2 == sc::FP< 3 > {21, 19, 9});
+                CHECK(local1 == one::FP< 3 > {32, 19, 10});
+                CHECK(local2 == one::FP< 3 > {21, 19, 9});
             }
 
             THEN("The point can be converted back to global coordinates") {
@@ -90,16 +90,16 @@ SCENARIO( "Converting between global and local coordinates" ) {
 }
 
 TEST_CASE("Generate the fragments capturing an inline") {
-    auto cube = sc::gvt< 3  >(
+    auto cube = one::gvt< 3  >(
         { 9, 15, 23 },
         { 3,  9,  5 }
     );
 
-    CHECK(cube.fragment_count(sc::dimension< 3 >{0}) == 3);
-    CHECK(cube.fragment_count(sc::dimension< 3 >{1}) == 2);
-    CHECK(cube.fragment_count(sc::dimension< 3 >{2}) == 5);
+    CHECK(cube.fragment_count(one::dimension< 3 >{0}) == 3);
+    CHECK(cube.fragment_count(one::dimension< 3 >{1}) == 2);
+    CHECK(cube.fragment_count(one::dimension< 3 >{2}) == 5);
 
-    const auto result = cube.slice(sc::dimension< 3 >{0}, 0);
+    const auto result = cube.slice(one::dimension< 3 >{0}, 0);
     const auto expected = decltype(result) {
         { 0, 0, 0 },
         { 0, 0, 1 },
@@ -117,16 +117,16 @@ TEST_CASE("Generate the fragments capturing an inline") {
 }
 
 TEST_CASE("Generate the fragments capturing a crossline") {
-    auto cube = sc::gvt< 3 > {
+    auto cube = one::gvt< 3 > {
         { 9, 15, 23 },
         { 3,  9,  5 },
     };
 
-    CHECK(cube.fragment_count(sc::dimension< 3 >{0}) == 3);
-    CHECK(cube.fragment_count(sc::dimension< 3 >{1}) == 2);
-    CHECK(cube.fragment_count(sc::dimension< 3 >{2}) == 5);
+    CHECK(cube.fragment_count(one::dimension< 3 >{0}) == 3);
+    CHECK(cube.fragment_count(one::dimension< 3 >{1}) == 2);
+    CHECK(cube.fragment_count(one::dimension< 3 >{2}) == 5);
 
-    const auto result = cube.slice(sc::dimension< 3 >{1}, 11);
+    const auto result = cube.slice(one::dimension< 3 >{1}, 11);
     const auto expected = decltype(result) {
         { 0, 1, 0 },
         { 0, 1, 1 },
@@ -151,16 +151,16 @@ TEST_CASE("Generate the fragments capturing a crossline") {
 }
 
 TEST_CASE("Generate the fragments capturing a time slice") {
-    auto cube = sc::gvt< 3 > {
+    auto cube = one::gvt< 3 > {
         { 9, 15, 23 },
         { 3,  9,  5 },
     };
 
-    CHECK(cube.fragment_count(sc::dimension< 3 >{0}) == 3);
-    CHECK(cube.fragment_count(sc::dimension< 3 >{1}) == 2);
-    CHECK(cube.fragment_count(sc::dimension< 3 >{2}) == 5);
+    CHECK(cube.fragment_count(one::dimension< 3 >{0}) == 3);
+    CHECK(cube.fragment_count(one::dimension< 3 >{1}) == 2);
+    CHECK(cube.fragment_count(one::dimension< 3 >{2}) == 5);
 
-    const auto result = cube.slice(sc::dimension< 3 >{2}, 17);
+    const auto result = cube.slice(one::dimension< 3 >{2}, 17);
     const auto expected = decltype(result) {
         { 0, 0, 3 },
         { 0, 1, 3 },
@@ -175,21 +175,21 @@ TEST_CASE("Generate the fragments capturing a time slice") {
 }
 
 TEST_CASE("Figure out an global offset [0, len(survey)) from a point") {
-    const auto cube = sc::CS< 3 >(9, 15, 23);
+    const auto cube = one::CS< 3 >(9, 15, 23);
     const auto expected = 2495;
-    const auto p = sc::CP< 3 >(7, 3, 11);
+    const auto p = one::CP< 3 >(7, 3, 11);
     auto result = cube.to_offset(p);
     CHECK(result == expected);
 }
 
 TEST_CASE("fragment-id string generation") {
-    const auto id = sc::FID< 3 >(3, 5, 7);
+    const auto id = one::FID< 3 >(3, 5, 7);
     CHECK("3-5-7" == id.string());
 }
 
 namespace {
 
-const auto exdims = sc::FS< 3 >(3, 5, 7);
+const auto exdims = one::FS< 3 >(3, 5, 7);
 const auto exfragment = std::vector< unsigned char > {
     0x0, 0x0, 0x0, 0x0,
     0x0, 0x0, 0x1, 0x0,
@@ -314,7 +314,7 @@ const auto exfragment = std::vector< unsigned char > {
 
 }
 
-std::vector< unsigned char > slice(sc::slice_layout stride, std::size_t pin) {
+std::vector< unsigned char > slice(one::slice_layout stride, std::size_t pin) {
     auto outcome = std::vector< unsigned char >();
     const auto start = pin * stride.initial_skip;
     const auto superstride = stride.superstride * sizeof(float);
@@ -347,7 +347,7 @@ TEST_CASE("Extracting a dimension-0 slice from a fragment") {
     }();
 
     const auto pin = 1;
-    const auto stride = exdims.slice_stride(sc::dimension< 3 >(0));
+    const auto stride = exdims.slice_stride(one::dimension< 3 >(0));
     const auto outcome = slice(stride, pin);
     CHECK_THAT(outcome, Equals(expected));
 }
@@ -367,7 +367,7 @@ TEST_CASE("Extracting a dimension-1 slice from a fragment") {
     }();
 
     const auto pin = 1;
-    const auto stride = exdims.slice_stride(sc::dimension< 3 >(1));
+    const auto stride = exdims.slice_stride(one::dimension< 3 >(1));
     const auto outcome = slice(stride, pin);
     CHECK_THAT(outcome, Equals(expected));
 }
@@ -387,7 +387,7 @@ TEST_CASE("Extracting a dimension-2 slice from a fragment") {
     }();
 
     const auto pin = 1;
-    const auto stride = exdims.slice_stride(sc::dimension< 3 >(2));
+    const auto stride = exdims.slice_stride(one::dimension< 3 >(2));
     const auto outcome = slice(stride, pin);
     CHECK_THAT(outcome, Equals(expected));
 }
@@ -470,10 +470,10 @@ TEST_CASE("Put a fragment slice into a cube slice (dimension 0)") {
         0x0, 0x0, 0x0, 0x0,
     };
 
-    const auto dim0 = sc::dimension< 3 >(0);
-    const auto slice_frag_dim = sc::FS< 3 > { 1, 5,  7 };
-    const auto slice_dim      = sc::CS< 3 > { 1, 5, 14 };
-    auto gvt = sc::gvt< 3 >(slice_dim, slice_frag_dim);
+    const auto dim0 = one::dimension< 3 >(0);
+    const auto slice_frag_dim = one::FS< 3 > { 1, 5,  7 };
+    const auto slice_dim      = one::CS< 3 > { 1, 5, 14 };
+    auto gvt = one::gvt< 3 >(slice_dim, slice_frag_dim);
     REQUIRE(expected.size() == gvt.global_size() * sizeof(float));
 
     /* extract a slice from a fragment */
@@ -485,7 +485,7 @@ TEST_CASE("Put a fragment slice into a cube slice (dimension 0)") {
     out.assign(expected.size(), 0);
 
     /* Put the slice tile at the right place in the output array */
-    const auto id = sc::FID< 3 > { 0, 0, 0 };
+    const auto id = one::FID< 3 > { 0, 0, 0 };
     auto layout = gvt.slice_stride(dim0, id);
     auto src = source.begin();
     auto dst = out.begin() + layout.initial_skip * sizeof(float);
@@ -546,10 +546,10 @@ TEST_CASE("Put a fragment slice into a cube slice (dimension 1)") {
         0x2, 0x1, 0x6, 0x0,
     };
 
-    const auto dim1 = sc::dimension< 3 >(1);
-    const auto slice_frag_dim = sc::FS< 3 > { 3, 1,  7 };
-    const auto slice_dim      = sc::CS< 3 > { 3, 1, 14 };
-    auto gvt = sc::gvt< 3 >(slice_dim, slice_frag_dim);
+    const auto dim1 = one::dimension< 3 >(1);
+    const auto slice_frag_dim = one::FS< 3 > { 3, 1,  7 };
+    const auto slice_dim      = one::CS< 3 > { 3, 1, 14 };
+    auto gvt = one::gvt< 3 >(slice_dim, slice_frag_dim);
     REQUIRE(expected.size() == gvt.global_size() * sizeof(float));
 
     /* extract a slice from a fragment */
@@ -561,7 +561,7 @@ TEST_CASE("Put a fragment slice into a cube slice (dimension 1)") {
     out.assign(expected.size(), 0);
 
     /* Put the slice tile at the right place in the output array */
-    const auto id = sc::FID< 3 > { 0, 0, 1 };
+    const auto id = one::FID< 3 > { 0, 0, 1 };
     auto layout = gvt.slice_stride(dim1, id);
     auto src = source.begin();
     auto dst = out.begin() + layout.initial_skip * sizeof(float);
@@ -621,10 +621,10 @@ TEST_CASE("Put a fragment slice into a cube slice (dimension 1, lateral)") {
         0x2, 0x1, 0x6, 0x0,
     };
 
-    const auto dim1 = sc::dimension< 3 >(1);
-    const auto slice_frag_dim = sc::FS< 3 > { 3, 1, 7 };
-    const auto slice_dim      = sc::CS< 3 > { 6, 1, 7 };
-    auto gvt = sc::gvt< 3 >(slice_dim, slice_frag_dim);
+    const auto dim1 = one::dimension< 3 >(1);
+    const auto slice_frag_dim = one::FS< 3 > { 3, 1, 7 };
+    const auto slice_dim      = one::CS< 3 > { 6, 1, 7 };
+    auto gvt = one::gvt< 3 >(slice_dim, slice_frag_dim);
     REQUIRE(expected.size() == gvt.global_size() * sizeof(float));
 
     /* extract a slice from a fragment */
@@ -636,7 +636,7 @@ TEST_CASE("Put a fragment slice into a cube slice (dimension 1, lateral)") {
     out.assign(expected.size(), 0);
 
     /* Put the slice tile at the right place in the output array */
-    const auto id = sc::FID< 3 > { 1, 0, 0 };
+    const auto id = one::FID< 3 > { 1, 0, 0 };
     auto layout = gvt.slice_stride(dim1, id);
     auto src = source.begin();
     auto dst = out.begin() + layout.initial_skip * sizeof(float);
@@ -684,10 +684,10 @@ TEST_CASE("Put a fragment slice into a cube slice (dimension 2)") {
         0x2, 0x4, 0x1, 0x0,
     };
 
-    const auto dim2 = sc::dimension< 3 >(2);
-    const auto slice_frag_dim = sc::FS< 3 > { 3, 5, 1 };
-    const auto slice_dim      = sc::CS< 3 > { 6, 5, 1 };
-    auto gvt = sc::gvt< 3 >(slice_dim, slice_frag_dim);
+    const auto dim2 = one::dimension< 3 >(2);
+    const auto slice_frag_dim = one::FS< 3 > { 3, 5, 1 };
+    const auto slice_dim      = one::CS< 3 > { 6, 5, 1 };
+    auto gvt = one::gvt< 3 >(slice_dim, slice_frag_dim);
     REQUIRE(expected.size() == gvt.global_size() * sizeof(float));
 
     /* extract a slice from a fragment */
@@ -699,7 +699,7 @@ TEST_CASE("Put a fragment slice into a cube slice (dimension 2)") {
     out.assign(expected.size(), 0);
 
     /* Put the slice tile at the right place in the output array */
-    const auto id = sc::FID< 3 > { 1, 0, 0 };
+    const auto id = one::FID< 3 > { 1, 0, 0 };
     auto layout = gvt.slice_stride(dim2, id);
     auto src = source.begin();
     auto dst = out.begin() + layout.initial_skip * sizeof(float);
