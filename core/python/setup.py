a5fb1ad586336acf592213ecb35a4ddd8359b4fe
Anette Uttisrud <autt@equinor.com>
2020-03-09 12:12:45 +0100

    Add oneseismic.geometry python extension
    The bulk of this patch is the build system (setup.py and cmake), which
    are largely taken from ecl3 [1]. The oneseismic.geometry exposes the 3D
    CS, FS, and GVT datatypes, and virtually no operations.
    
    [1] https://github.com/equinor/ecl3

---
+++ core/python/setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import os
+import skbuild
+import setuptools
+
+class get_pybind_include(object):
+    def __init__(self, user=False):
+        self.user = user
+
+    def __str__(self):
+        # postpone importing pybind11 until building actually happens
+        import pybind11
+        return pybind11.get_include(self.user)
+
+def src(x):
+    root = os.path.dirname( __file__ )
+    return os.path.abspath(os.path.join(root, x))
+
+pybind_includes = [
+    str(get_pybind_include()),
+    str(get_pybind_include(user = True))
+]
+
+skbuild.setup(
+    name = 'oneseismic',
+    description = 'oneseismic',
+    long_description = 'oneseismic',
+    url = 'https://github.com/equinor/seismic-cloud',
+    packages = [
+        'oneseismic',
+    ],
+    license = 'AGPL-3.0',
+    platforms = 'any',
+    install_requires = [
+    ],
+    setup_requires = [
+        'setuptools >= 28',
+        'pybind11 >= 2.2',
+        'pytest-runner',
+    ],
+    tests_require = [
+        'pytest',
+    ],
+    # we're building with the pybind11 fetched from pip. Since we don't rely on
+    # a cmake-installed pybind there's also no find_package(pybind11) -
+    # instead, the get include dirs from the package and give directly from
+    # here
+    cmake_args = [
+        '-DPYBIND11_INCLUDE_DIRS=' + ';'.join(pybind_includes),
+        # we can safely pass OSX_DEPLOYMENT_TARGET as it's ignored on
+        # everything not OS X. We depend on C++11, which makes our minimum
+        # supported OS X release 10.9
+        '-DCMAKE_OSX_DEPLOYMENT_TARGET=10.9',
+    ],
+    cmake_source_dir = 'oneseismic',
+    # skbuild's test imples develop, which is pretty obnoxious instead, use a
+    # manually integrated pytest.
+    cmdclass = { 'test': setuptools.command.test.test },
+)
