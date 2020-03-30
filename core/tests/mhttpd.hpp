1d842dc612875a124fe107d78d4f77a07aacf863
Jørgen Kvalsvik <jokva@equinor.com>
2020-03-30 11:26:16 +0200

    Remove virtual from dervied class in mhttpd.hpp

--- core/tests/mhttpd.hpp
+++ core/tests/mhttpd.hpp
@@ -70,7 +70,7 @@ private:
 struct loopback_cfg : public one::storage_configuration {
     explicit loopback_cfg(int port) : port(port) {}
 
-    virtual std::string url(
+    std::string url(
             const one::batch&,
             const std::string&) const override {
         return "http://127.0.0.1:" + std::to_string(this->port);
