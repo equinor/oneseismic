69bcb9323eafcf26cd1bfc5175082bb1e641dd33
Kjell W. Kongsvik <kjell.kongsvik@gmail.com>
2020-06-22 09:43:20 +0200

    Cleaner openidConfiguration
    More private types.
    Only include fields in jwk/openIDConfig structs that are used.
    We only support RSA from openid -> more explicit.
    Simplify testing of openid configuration -> mock http client get.
    Update go packages.

--- api/auth/openidConf_test.go
+++ api/auth/openidConf_test.go
@@ -1,122 +1,45 @@
 package auth
 
 import (
-	"crypto/rsa"
-	"fmt"
+	"bytes"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
-	"os"
-	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 var testAuthServer *httptest.Server
 var testAuthServerURL string
 
-func mockResponses(r *http.Request) string {
-	switch r.URL.String() {
-	case "/.well-known/openid-configuration":
-		return fmt.Sprintf(`
-		{
-			"jwks_uri":"http://%s/jwks"
-		}`, r.Host)
-	case "/jwks":
-		return `
-		{
-			"keys": [
-			  {
-				"alg": "RS256",
-				"kty": "RSA",
-				"use": "sig",
-				"x5c": [
-				  "MIIC+DCCAeCgAwIBAgIJBIGjYW6hFpn2MA0GCSqGSIb3DQEBBQUAMCMxITAfBgNVBAMTGGN1c3RvbWVyLWRlbW9zLmF1dGgwLmNvbTAeFw0xNjExMjIyMjIyMDVaFw0zMDA4MDEyMjIyMDVaMCMxITAfBgNVBAMTGGN1c3RvbWVyLWRlbW9zLmF1dGgwLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMnjZc5bm/eGIHq09N9HKHahM7Y31P0ul+A2wwP4lSpIwFrWHzxw88/7Dwk9QMc+orGXX95R6av4GF+Es/nG3uK45ooMVMa/hYCh0Mtx3gnSuoTavQEkLzCvSwTqVwzZ+5noukWVqJuMKNwjL77GNcPLY7Xy2/skMCT5bR8UoWaufooQvYq6SyPcRAU4BtdquZRiBT4U5f+4pwNTxSvey7ki50yc1tG49Per/0zA4O6Tlpv8x7Red6m1bCNHt7+Z5nSl3RX/QYyAEUX1a28VcYmR41Osy+o2OUCXYdUAphDaHo4/8rbKTJhlu8jEcc1KoMXAKjgaVZtG/v5ltx6AXY0CAwEAAaMvMC0wDAYDVR0TBAUwAwEB/zAdBgNVHQ4EFgQUQxFG602h1cG+pnyvJoy9pGJJoCswDQYJKoZIhvcNAQEFBQADggEBAGvtCbzGNBUJPLICth3mLsX0Z4z8T8iu4tyoiuAshP/Ry/ZBnFnXmhD8vwgMZ2lTgUWwlrvlgN+fAtYKnwFO2G3BOCFw96Nm8So9sjTda9CCZ3dhoH57F/hVMBB0K6xhklAc0b5ZxUpCIN92v/w+xZoz1XQBHe8ZbRHaP1HpRM4M7DJk2G5cgUCyu3UBvYS41sHvzrxQ3z7vIePRA4WF4bEkfX12gvny0RsPkrbVMXX1Rj9t6V7QXrbPYBAO+43JvDGYawxYVvLhz+BJ45x50GFQmHszfY3BR9TPK8xmMmQwtIvLu1PMttNCs7niCYkSiUv2sc2mlq1i3IashGkkgmo="
-				],
-				"n": "yeNlzlub94YgerT030codqEztjfU_S6X4DbDA_iVKkjAWtYfPHDzz_sPCT1Axz6isZdf3lHpq_gYX4Sz-cbe4rjmigxUxr-FgKHQy3HeCdK6hNq9ASQvMK9LBOpXDNn7mei6RZWom4wo3CMvvsY1w8tjtfLb-yQwJPltHxShZq5-ihC9irpLI9xEBTgG12q5lGIFPhTl_7inA1PFK97LuSLnTJzW0bj096v_TMDg7pOWm_zHtF53qbVsI0e3v5nmdKXdFf9BjIARRfVrbxVxiZHjU6zL6jY5QJdh1QCmENoejj_ytspMmGW7yMRxzUqgxcAqOBpVm0b-_mW3HoBdjQ",
-				"e": "AQAB",
-				"kid": "NjVBRjY5MDlCMUIwNzU4RTA2QzZFMDQ4QzQ2MDAyQjVDNjk1RTM2Qg",
-				"x5t": "NjVBRjY5MDlCMUIwNzU4RTA2QzZFMDQ4QzQ2MDAyQjVDNjk1RTM2Qg"
-			  }
-			]}`
+func mockGet(url string) (*http.Response, error) {
+	jwksURI := `{"jwks_uri":"jwks"}`
+	// no need to include e, n in test; they will become 0
+	keys := `
+	{
+		"keys": [
+		  {
+			"kty": "RSA",
+			"kid": "kid"
+		  }
+		]
+	}`
+	json := jwksURI
+	if url == "jwks" {
+		json = keys
 	}
 
-	return ""
+	return &http.Response{
+		StatusCode: 200,
+		Body:       ioutil.NopCloser(bytes.NewReader([]byte(json))),
+	}, nil
 }
 
-func TestMain(m *testing.M) {
+func TestGetRSAKeys(t *testing.T) {
+	httpGet = mockGet
 
-	testAuthServer = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-		resp := mockResponses(r)
-		if len(resp) == 0 {
-			w.WriteHeader(404)
-		}
-
-		fmt.Fprintln(w, resp)
-	}))
-	testAuthServerURL = testAuthServer.URL + "/.well-known/openid-configuration"
-	r := m.Run()
-	testAuthServer.Close()
-	os.Exit(r)
-}
-
-func TestGetKey(t *testing.T) {
-	type args struct {
-		authserver string
-	}
-	e, err := fromB64("AQAB")
-	if err != nil {
-		t.Errorf("fromB64 failed: error = %v", err)
-		return
-	}
-	n, err := fromB64("yeNlzlub94YgerT030codqEztjfU_S6X4DbDA_iVKkjAWtYfPHDzz_sPCT1Axz6isZdf3lHpq_gYX4Sz-cbe4rjmigxUxr-FgKHQy3HeCdK6hNq9ASQvMK9LBOpXDNn7mei6RZWom4wo3CMvvsY1w8tjtfLb-yQwJPltHxShZq5-ihC9irpLI9xEBTgG12q5lGIFPhTl_7inA1PFK97LuSLnTJzW0bj096v_TMDg7pOWm_zHtF53qbVsI0e3v5nmdKXdFf9BjIARRfVrbxVxiZHjU6zL6jY5QJdh1QCmENoejj_ytspMmGW7yMRxzUqgxcAqOBpVm0b-_mW3HoBdjQ")
-	if err != nil {
-		t.Errorf("fromB64 failed: error = %v", err)
-		return
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    interface{}
-		wantErr bool
-	}{
-		{"nil auth server", args{}, nil, true},
-		{"test auth server", args{testAuthServerURL},
-			map[string]interface{}{
-				"NjVBRjY5MDlCMUIwNzU4RTA2QzZFMDQ4QzQ2MDAyQjVDNjk1RTM2Qg": &rsa.PublicKey{N: &n, E: int(e.Int64())}},
-			false},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := GetOIDCKeySet(tt.args.authserver)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("GetOIDCKeySet() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-
-			if !reflect.DeepEqual(got, tt.want) && !tt.wantErr {
-				t.Errorf("GetOIDCKeySet() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_getJSON(t *testing.T) {
-	type args struct {
-		url    string
-		target interface{}
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := getJSON(tt.args.url, tt.args.target); (err != nil) != tt.wantErr {
-				t.Errorf("getJSON() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
+	key, err := GetRSAKeys("")
+	assert.Nil(t, err)
+	assert.Len(t, key, 1)
 }
