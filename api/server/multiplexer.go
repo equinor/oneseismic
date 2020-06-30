4d7598eb334ebd1caa3225c60acd4ca6f9516553
Jørgen Kvalsvik <jokva@equinor.com>
2020-06-30 12:22:14 +0200

    Formalize multiplexer/jobs protocol
    Add helper functions that formalizes and provides a single point for the
    protocol and message format used to communicate with the next step in
    the pipeline [1]. While it adds no new behaviour, it should work well as
    a more obvious place to make modifications to the protocol, without
    having to extend and reverse engineer raw ZMQ or chan message passing.
    
    [1] in this case, the manifest-fetch

--- api/server/multiplexer.go
+++ api/server/multiplexer.go
@@ -12,6 +12,54 @@ type job struct {
 	reply   chan []byte
 }
 
+type wire interface {
+	loadZMQ(msg [][]byte)
+	sendZMQ(socket *zmq.Socket) (total int, err error)
+}
+
+/*
+ * The partitionRequest is the message sent from this (the session manager)
+ * over ZMQ to the scheduler/job partitioner, which decides what data to
+ * retrieve from storage (manifest-server in current vocabulary)
+ */
+type partitionRequest struct {
+	// Return-address that flows through to make sure that data is returned to
+	// the correct node that manages the session
+	address string
+	jobID string
+	request []byte
+}
+
+/*
+ * The make/send functions are stupid helpers to help formalise the protocol
+ * for communication with other parts of oneseismic, and provide a canonical
+ * way of formatting messages for both the wire (over ZMQ) and over go channels
+ */
+func newPartitionRequest(j *job, address string) *partitionRequest {
+	return &partitionRequest {
+		address: address,
+		jobID: j.jobID,
+		request: j.request,
+	}
+}
+
+func (p *partitionRequest) sendZMQ(socket *zmq.Socket) (total int, err error) {
+	return socket.SendMessage(p.address, p.jobID, p.request)
+}
+
+/*
+ * Parse a partition request as it is delivered in a ZMQ multipart message.
+ * While this currently has no error checking, or really does anything
+ * sophisticated, it's the canonical way to obtain a partitionRequest from a
+ * multipart message, and *the* go reference for what the messages from the
+ * fragment/worker looks like.
+ */
+func (p *partitionRequest) loadZMQ(msg [][]byte) {
+	p.address = string(msg[0])
+	p.jobID = string(msg[1])
+	p.request = msg[2]
+}
+
 func multiplexer(jobs chan job, address string, reqNdpt string, repNdpt string) {
 	req, err := zmq.NewSocket(zmq.PUSH)
 
@@ -56,10 +104,9 @@ func multiplexer(jobs chan job, address string, reqNdpt string, repNdpt string) 
 			rc <- msg
 			delete(replyChnls, jobID)
 		case j := <-jobs:
+			part := newPartitionRequest(&j, address)
 			replyChnls[j.jobID] = j.reply
-			req.Send(address, zmq.SNDMORE)
-			req.Send(j.jobID, zmq.SNDMORE)
-			req.SendMessage(j.request)
+			part.sendZMQ(req)
 		}
 	}
 }
