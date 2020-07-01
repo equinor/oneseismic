3d13e9c287efa5253dbc249a310c582693ee60d2
Kjell W. Kongsvik <kjell.kongsvik@gmail.com>
2020-07-01 10:42:34 +0200

    Simpler api config
    simply use os env in main, no config struct needed

--- api/main.go
+++ api/main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"crypto/rsa"
-	"fmt"
-	"log"
 	"os"
 	"strings"
 
@@ -29,79 +26,41 @@ func init() {
 //@in header
 //@name Authorization
 func main() {
-	c, err := getConfig()
-	if err != nil {
-		log.Fatal("Failed to load config", err)
-	}
-
-	err = serve(c)
-	if err != nil {
-		log.Fatalf("failed to start server: %v", err)
-	}
-}
-
-func serve(c *config) error {
 	logLevel := os.Getenv("LOG_LEVEL")
+
 	golog.SetTimeFormat("")
 	golog.SetLevel(logLevel)
 
-	app := iris.Default()
-
-	app.Logger().SetLevel(logLevel)
-
-	rsaKeys, err := auth.GetRSAKeys(c.authServer + "/.well-known/openid-configuration")
+	rsaKeys, err := auth.GetRSAKeys(os.Getenv("AUTHSERVER") + "/.well-known/openid-configuration")
 	if err != nil {
-		return fmt.Errorf("could not get keyset: %w", err)
+		golog.Error("could not get keyset", err)
 	}
-	app.Use(auth.CheckJWT(rsaKeys, c.apiSecret))
-	app.Use(auth.ValidateIssuer(c.issuer))
 
+	app := iris.Default()
+	app.Logger().SetLevel(logLevel)
+	app.Use(auth.CheckJWT(rsaKeys, []byte(os.Getenv("API_SECRET"))))
+	app.Use(auth.ValidateIssuer(os.Getenv("ISSUER")))
 	app.Use(iris.Gzip)
 	enableSwagger(app)
 
-	if err := server.Register(
+	err = server.Register(
 		app,
-		c.storageURL,
-		c.accountName,
-		c.accountKey,
-		c.zmqReqAddr,
-		c.zmqRepAddr,
-	); err != nil {
-		return fmt.Errorf("register endpoints: %w", err)
+		strings.ReplaceAll(os.Getenv("AZURE_STORAGE_URL"), "{}", "%s"),
+		os.Getenv("AZURE_STORAGE_ACCOUNT"),
+		os.Getenv("AZURE_STORAGE_ACCESS_KEY"),
+		os.Getenv("ZMQ_REQ_ADDR"),
+		os.Getenv("ZMQ_REP_ADDR"),
+	)
+	if err != nil {
+		golog.Error("register endpoints: %w", err)
 	}
 
-	return app.Run(iris.Addr(c.hostAddr))
+	err = app.Run(iris.Addr(os.Getenv("HOST_ADDR")))
+	if err != nil {
+		golog.Fatal(err)
+	}
 }
 
 func enableSwagger(app *iris.Application) {
 	app.Get("/swagger/{any:path}", swagger.WrapHandler(swaggerFiles.Handler))
-}
-
-type config struct {
-	hostAddr    string
-	storageURL  string
-	accountName string
-	accountKey  string
-	authServer  string
-	issuer      string
-	apiSecret   []byte
-	zmqReqAddr  string
-	zmqRepAddr  string
-	SigKeySet   map[string]rsa.PublicKey
-}
-
-func getConfig() (*config, error) {
-	conf := &config{
-		authServer:  os.Getenv("AUTHSERVER"),
-		apiSecret:   []byte(os.Getenv("API_SECRET")),
-		issuer:      os.Getenv("ISSUER"),
-		storageURL:  strings.ReplaceAll(os.Getenv("AZURE_STORAGE_URL"), "{}", "%s"),
-		accountName: os.Getenv("AZURE_STORAGE_ACCOUNT"),
-		accountKey:  os.Getenv("AZURE_STORAGE_ACCESS_KEY"),
-		hostAddr:    os.Getenv("HOST_ADDR"),
-		zmqRepAddr:  os.Getenv("ZMQ_REP_ADDR"),
-		zmqReqAddr:  os.Getenv("ZMQ_REQ_ADDR"),
-	}
-
-	return conf, nil
 }
