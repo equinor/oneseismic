c80217279a9a9fdcc54c3f903605da3af10abcb5
Jørgen Kvalsvik <jokva@equinor.com>
2020-07-10 17:32:44 +0200

    Use a single map for process status
    Use a single PID -> struct map, rather than a set of maps for storing
    the process status. This makes it easier to add process metadata, and
    gives fewer confusing names in the mix.

--- api/server/multiplexer.go
+++ api/server/multiplexer.go
@@ -194,31 +194,36 @@ func (s *sessions) Run(address string, reqNdpt string, repNdpt string) {
 		}
 	}()
 
+	type procstatus struct {
+		/*
+		 * Channel to send replies (read from ZMQ) to, to be assembled and
+		 * returned to users
+		 */
+		out chan partialResult
+		/*
+		 * Bit-array of already-received parts for this process
+		 */
+		completed []bool
+	}
+
 	// TODO: Clean up in case a reply never arrives?
-	/*
-	 * Store pid -> channel to send result
-	 */
-	processes := make(map[string]chan partialResult)
-	progress := make(map[string][]bool)
+	processes := make(map[string]procstatus)
 
 	for {
 		select {
 		case r := <-rep:
-			rc := processes[r.pid]
-			prog := progress[r.pid]
+			proc := processes[r.pid]
 
-			if prog == nil {
-				progress[r.pid] = make([]bool, r.m)
-				prog = progress[r.pid]
+			if proc.completed == nil {
+				proc.completed = make([]bool, r.m)
 			}
 
-			rc <- r
-			prog[r.n] = true
+			proc.out <- r
+			proc.completed[r.n] = true
 
-			if all(prog) {
-				close(rc)
+			if all(proc.completed) {
+				close(proc.out)
 				delete(processes, r.pid)
-				delete(progress, r.pid)
 			}
 
 		case j := <-s.queue:
@@ -227,8 +232,10 @@ func (s *sessions) Run(address string, reqNdpt string, repNdpt string) {
 				pid: j.jobID,
 				request: j.request,
 			}
-			processes[j.jobID] = j.reply
-			progress[j.jobID] = nil
+			processes[j.jobID] = procstatus {
+				out: j.reply,
+				completed: nil,
+			}
 			proc.sendZMQ(req)
 		}
 	}
