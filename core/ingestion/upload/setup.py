36dd665540ffab64f9660c8e207ce60aa80adb67
Jørgen Kvalsvik <jokva@equinor.com>
2020-07-21 18:49:36 +0200

    Add minimal readme and package metadata in upload

--- core/ingestion/upload/setup.py
+++ core/ingestion/upload/setup.py
@@ -1,7 +1,16 @@
 import setuptools
 
+with open('readme') as f:
+    long_description = f.read()
+
 setuptools.setup(
-    name = 'upload',
+    name = 'oneseismic-upload',
+    description = 'oneseismic - upload cubes to storage',
+    long_description = long_description,
+
+    url = 'https://github.com/equinor/oneseismic',
+    license = 'AGPL-3.0',
+    version = '0.1.0',
 
     packages = [
         'upload',
