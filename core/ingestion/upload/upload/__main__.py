36cc36649c0949069b3e45bdb4c30d5df19d2d4a
Jørgen Kvalsvik <jokva@equinor.com>
2020-07-21 16:37:59 +0200

    Beef up upload --help, add short-opts
    Beef up the upload --help and add a few short opts, to make it slightly
    more self-descriptive and faster to use.

--- core/ingestion/upload/upload/__main__.py
+++ core/ingestion/upload/upload/__main__.py
@@ -7,12 +7,37 @@ from azure.storage.blob import BlobServiceClient
 from .upload import upload
 
 def main(argv):
-    parser = argparse.ArgumentParser('Ingest SEG-Y')
+    dimhelp = 'fragment size (samples) in {} direction'
+    parser = argparse.ArgumentParser(
+        prog = 'oneseismic-upload',
+        description = 'Upload cubes to oneseismic storage',
+        epilog = '%(prog)s relies on azure connection strings, see {}'.format(
+            'https://docs.microsoft.com/azure/storage/common/storage-configure-connection-string'
+        ),
+    )
     parser.add_argument('meta', type = str, help = 'metadata json')
     parser.add_argument('input', type = str, help = 'input SEG-Y file')
-    parser.add_argument('--subcube-dim-0', type = int, default = 120)
-    parser.add_argument('--subcube-dim-1', type = int, default = 120)
-    parser.add_argument('--subcube-dim-2', type = int, default = 120)
+    parser.add_argument(
+        '--subcube-dim-0', '-i',
+        type = int,
+        default = 120,
+        metavar = 'I',
+        help = dimhelp.format('X'),
+    )
+    parser.add_argument(
+        '--subcube-dim-1', '-j',
+        type = int,
+        default = 120,
+        metavar = 'J',
+        help = dimhelp.format('Y'),
+    )
+    parser.add_argument(
+        '--subcube-dim-2', '-k',
+        type = int,
+        default = 120,
+        metavar = 'K',
+        help = dimhelp.format('Z'),
+    )
     args = parser.parse_args(argv)
 
     params = {
