15a52c51d64a176f9a7ea122c57c54ea464e37ab
Kjell W. Kongsvik <kjell.kongsvik@gmail.com>
2020-08-04 11:08:55 +0200

    Do not upgrade packages when running go generate
    go.mod and go.sum should not be considered artifacts.
    The swag and protoc-gen-go commands need to be installed during build.
    Their versions should respect go.mod and go.sum.
    The "go get" command gets the last version of the package and updates
    go.mod and go.sum.
    The "go install" command seem to behave as we need.

--- api/generate.go
+++ api/generate.go
@@ -1,10 +1,10 @@
 package main
 
 //Swagger doc Generate
-//go:generate go get github.com/swaggo/swag/cmd/swag
+//go:generate go install github.com/swaggo/swag/cmd/swag
 //go:generate swag init
 
 //protobuf generation
 //go:generate mkdir -p oneseismic
-//go:generate go get google.golang.org/protobuf/cmd/protoc-gen-go
+//go:generate go install google.golang.org/protobuf/cmd/protoc-gen-go
 //go:generate protoc -I ../protos ../protos/core.proto --go_out=oneseismic
