662b73e34d8c1bfc1316d043bd486c94d38ca3e7
Kjell W. Kongsvik <kjell.kongsvik@gmail.com>
2020-09-14 12:59:22 +0200

    Remove script to generate python sdk from swagger
    The python api needs to be built manually

--- generate_sdk.sh
+++
@@ -1,5 +0,0 @@
-pushd api
-go generate
-popd
-# java -jar ./swagger-codegen-cli-2.4.10.jar generate -l python -i api/docs/swagger.yaml -c api-codegen-config.json -o sdk/
-docker run --rm -v $(pwd):/local swaggerapi/swagger-codegen-cli generate -i /local/api/docs/swagger.yaml -l python -o /local/sdk -c /local/sdk/.swagger-codegen.json 
