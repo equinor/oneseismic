1c1e140968fc78080542c051a69d787748b3c04a
Kjell W. Kongsvik <kjell.kongsvik@gmail.com>
2020-09-14 13:14:36 +0200

    Remove swagger, manual doc needs to be added later
    Swagger do not create the documentation that is really needed.
    The api is very simple, usage and parsing of result requires manual
    documentation. May turn swagger on later. Just noise now.

--- api/server/storeController.go
+++ api/server/storeController.go
@@ -11,14 +11,6 @@ type storeController struct {
 	store
 }
 
-// @Summary list of cubes
-// @Description get list of available cubes
-// @ID list_cubes
-// @Produce  application/json
-// @Success 200 {array} string
-// @Failure 500 {string} string
-// @security ApiKeyAuth
-// @Router / [get]
 func (sc *storeController) list(ctx iris.Context) {
 	token, ok := ctx.Values().Get("jwt").(string)
 	if !ok {
@@ -38,17 +30,6 @@ func (sc *storeController) list(ctx iris.Context) {
 	}
 }
 
-// @Summary services for cube
-// @Description list services available on cube
-// @ID cube
-// @Produce application/json
-// @Param guid path string true "guid"
-// @Success 200 {array} string
-// @Failure 400 {string} string
-// @Failure 404 {string} string
-// @Failure 500 {string} string
-// @security ApiKeyAuth
-// @Router /{guid} [get]
 func (sc *storeController) services(ctx iris.Context) {
 	token, ok := ctx.Values().Get("jwt").(string)
 	if !ok {
@@ -73,17 +54,6 @@ func (sc *storeController) services(ctx iris.Context) {
 	}
 }
 
-// @Description list of dimensions in cube
-// @Summary show dimensions in slice
-// @ID dimensions
-// @Produce application/json
-// @Param guid path string true "guid"
-// @Success 200 {string} string
-// @Failure 400 {string} string
-// @Failure 404 {string} string
-// @Failure 500 {string} string
-// @security ApiKeyAuth
-// @Router /{guid}/slice [get]
 func (sc *storeController) dimensions(ctx iris.Context) {
 	token, ok := ctx.Values().Get("jwt").(string)
 	if !ok {
@@ -108,25 +78,13 @@ func (sc *storeController) dimensions(ctx iris.Context) {
 	}
 }
 
-// @Description list lines in dimension
-// @Summary show lines of a dimension in slice
-// @ID lines
-// @Produce application/json
-// @Param guid path string true "guid"
-// @Param dimension path int32 true "dimension"
-// @Success 200 {string} string
-// @Failure 400 {string} string
-// @Failure 404 {string} string
-// @Failure 500 {string} string
-// @security ApiKeyAuth
-// @Router /{guid}/slice/{dimension} [get]
 func (sc *storeController) lines(ctx iris.Context) {
 	token, ok := ctx.Values().Get("jwt").(string)
 	if !ok {
 		ctx.StatusCode(http.StatusInternalServerError)
 		return
 	}
-	
+
 	guid := ctx.Params().GetString("guid")
 	if len(guid) == 0 {
 		ctx.StatusCode(http.StatusBadRequest)
