b020e51cbb4863919fb3953b1f9aa2b80e911577
Kjell W. Kongsvik <kjell.kongsvik@gmail.com>
2020-09-18 08:53:51 +0200

    Merge sliceModel and sliceController into slicer
    sliceModel too small for separate file.
    Model and Controller names are not good.

--- api/server/slicer.go
+++ api/server/slicer.go
@@ -2,12 +2,14 @@ package server
 
 import (
 	"net/http"
+	"fmt"
 
 	"github.com/equinor/oneseismic/api/oneseismic"
 	"github.com/google/uuid"
 	"github.com/rs/zerolog/log"
 	"github.com/kataras/iris/v12"
 	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
 )
 
 type failure struct {
@@ -122,4 +124,93 @@ func (sc *sliceController) get(ctx iris.Context) {
 	}
 
 	return
+}
+
+type slicer struct {
+	endpoint string
+	sessions *sessions
+}
+
+func (s *slicer) fetchSlice(
+	guid string,
+	dim int32,
+	lineno int32,
+	requestid string,
+	token string,
+) (*oneseismic.SliceResponse, error) {
+
+	msg := oneseismic.ApiRequest {
+		Requestid:       requestid,
+		Token:           token,
+		Guid:            guid,
+		StorageEndpoint: s.endpoint,
+		Shape: &oneseismic.FragmentShape{
+			Dim0: 64,
+			Dim1: 64,
+			Dim2: 64,
+		},
+		Function: &oneseismic.ApiRequest_Slice{
+			Slice: &oneseismic.ApiSlice{
+				Dim:    dim,
+				Lineno: lineno,
+			},
+		},
+	}
+
+	req, err := proto.Marshal(&msg)
+	if err != nil {
+		return nil, fmt.Errorf("Marshalling oneseisimc.ApiRequest: %w", err)
+	}
+
+	proc := process{pid: requestid, request: req}
+	fr := oneseismic.FetchResponse{}
+
+	io := s.sessions.Schedule(&proc)
+
+	/*
+	 * Read and parse messages as they come, and consider the process complete
+	 * when the reply-channel closes.
+	 *
+	 * Right now, the result is assembled here and returned in one piece to
+	 * users, so it never looks like a parallelised job. This is so that we can
+	 * experiment with chunk sizes, worker nodes, load etc. without having to
+	 * be bothered with a more complex protocol between API and users, and so
+	 * that previously-written clients still work. In the future, this will
+	 * probably change and partial results will be transmitted.
+	 *
+	 * TODO: This gives weak failure handling, and Session needs a way to
+	 * signal failed processes
+	 */
+	var tiles []*oneseismic.SliceTile
+	for partial := range io.out {
+		err = proto.Unmarshal(partial.payload, &fr)
+
+		if err != nil {
+			return nil, fmt.Errorf("could not create slice response: %w", err)
+		}
+
+		slice := fr.GetSlice()
+		// TODO: cancel job on failure channel
+		if slice == nil {
+			switch x := fr.Function.(type) {
+			default:
+				msg := "%s Expected FetchResponse.Function = %T; was %T"
+				log.Error().Msgf(msg, requestid, slice, x)
+				return nil, fmt.Errorf("internal error")
+			}
+		}
+
+		tiles = append(tiles, slice.GetTiles()...)
+	}
+
+	/*
+	 * On successful runs, there are no messages on this channel, and the loop
+	 * turns into a no-op.
+	 */
+	 for failure := range io.err {
+		return nil, newFailure(failure)
+	}
+
+	fr.GetSlice().Tiles = tiles
+	return fr.GetSlice(), nil
 }
