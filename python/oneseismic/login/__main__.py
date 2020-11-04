76068428d1b69b634e45e8b678b8b8d5c30b2d9e
Jørgen Kvalsvik <jokva@equinor.com>
2020-11-04 15:01:55 +0100

    Add /config endpoint
    Add a /config enpoint which clients and users can GET to get a dict of
    instance-specific configuration parameters for tokens and scopes. The
    intention is to make login less painful, and make clients more
    self-repairing and adaptable for new deployments.
    
    The onseismic.login module is made aware of this, by making the
    --client-id, --auth-server, and --scopes arguments optional, and by
    adding a new positional parameter url. The url is the base-url for a
    oneseismic installation, and will set the options as specified by the
    running instance. The parameters can still be passed, and will override
    whatever the server provides.

--- python/oneseismic/login/__main__.py
+++ python/oneseismic/login/__main__.py
@@ -1,4 +1,6 @@
 import argparse
+import json
+import requests
 import sys
 
 from .login import login
@@ -8,13 +10,35 @@ def main(argv):
         prog = 'login',
         description = 'Log in to oneseismic',
     )
-    parser.add_argument('--client-id', type=str, required=True)
-    parser.add_argument('--auth-server', type=str, required=True)
-    parser.add_argument('--scopes', type=str, nargs='+', required=True)
+    parser.add_argument('--client-id', type=str,)
+    parser.add_argument('--auth-server', type=str)
+    parser.add_argument('--scopes', type=str, nargs='+')
     parser.add_argument('--cache-dir', type=str, required=False)
-
+    parser.add_argument('url', type = str, nargs = '?')
     args = parser.parse_args(argv)
-    login(args.client_id, args.auth_server, args.scopes, args.cache_dir)
+
+    config = {}
+    if args.url is not None:
+        authurl = f'{args.url}/config'
+        r = requests.get(authurl)
+        if r.status_code != 200:
+            msg = f'GET {authurl} status {r.status_code}: {r.content}'
+            raise RuntimeError(msg)
+        config.update(json.loads(r.content))
+
+    if args.client_id is not None:
+        config['client_id'] = args.client_id
+    if args.auth_server is not None:
+        config['authority'] = args.auth_server
+    if args.scopes is not None:
+        config['scopes'] = args.scopes
+
+    login(
+        client_id = config['client_id'],
+        auth_server = config['authority'],
+        scopes = config['scopes'],
+        cache_dir = args.cache_dir,
+    )
 
 if __name__ == '__main__':
     main((sys.argv[1:]))
