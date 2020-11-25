764255a9abeff77f7602df5033a5b68f784b0de7
Jørgen Kvalsvik <jokva@equinor.com>
2020-11-25 17:25:21 +0100

    Move GetManifest() to internal/util
    Getting and parsing the manifest is a prerequisite operation for all
    data queries. It is moved into the internal/util module so it is clearly
    available to other calls than just /slice.

--- api/api/slice.go
+++ api/api/slice.go
@@ -1,19 +1,17 @@
 package api
 
 import (
-	"context"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"strconv"
 
-	"github.com/Azure/azure-storage-blob-go/azblob"
 	"github.com/gin-gonic/gin"
 	"github.com/pebbe/zmq4"
 	"github.com/equinor/oneseismic/api/internal/auth"
 	"github.com/equinor/oneseismic/api/internal/message"
+	"github.com/equinor/oneseismic/api/internal/util"
 )
 
 type process struct {
@@ -104,88 +102,6 @@ func (s *Slice) makeTask(
 	}
 }
 
-/*
- * Get the manifest for the cube from the blob store.
- *
- * It's important that this is a blocking read, since this is the first
- * authorization mechanism in oneseismic. If the user (through the
- * on-behalf-token) does not have permissions to read the manifest, it
- * shouldn't be able to read the cube either. If so, no more processing should
- * be done, and the request discarded.
- */
-func getManifest(
-	ctx context.Context,
-	token string,
-	containerURL *url.URL,
-) ([]byte, error) {
-	credentials := azblob.NewTokenCredential(token, nil)
-	pipeline    := azblob.NewPipeline(credentials, azblob.PipelineOptions{})
-	container   := azblob.NewContainerURL(*containerURL, pipeline)
-	blob        := container.NewBlobURL("manifest.json")
-
-	dl, err := blob.Download(
-		ctx,
-		0, /* offset */
-		azblob.CountToEnd,
-		azblob.BlobAccessConditions {},
-		false, /* content-get-md5 */
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	body := dl.Body(azblob.RetryReaderOptions{})
-	defer body.Close()
-	return ioutil.ReadAll(body)
-}
-
-/*
- * Centralize the understanding of error conditions of azblob.download.
- *
- * There are two classes of errors:
- * 1. A hard failure, i.e. the request itself failed, such as network
- *    conditions.
- * 2. A soft failure, i.e. the request itself suceeded, but was without
- *    sufficient permissions, wrong syntax or similar. azblob probably handles
- *    this by parsing the status code and maybe the response body.
- *
- * Most calls to getManifest() should probably immediately call this function
- * on error and exit, but it's moved into its own function so that error paths
- * can be emulated and tested independently. An added benefit is that should a
- * function, for some reason, need getManifest() but want custom error+abort
- * handling, it is sufficient to implement bespoke error handling and simply
- * not call this.
- */
-func abortOnManifestError(ctx *gin.Context, err error) {
-	switch e := err.(type) {
-	case azblob.StorageError:
-		/*
-		 * Request successful, but the service returned some error e.g. a
-		 * non-existing cube, unauthorized request.
-		 *
-		 * For now, just write the status-text into the body, which should be
-		 * slightly more accurate than just the status code. Once the interface
-		 * matures, this should probably be a more structured error message.
-		 */
-		sc := e.Response()
-		ctx.String(sc.StatusCode, sc.Status)
-		ctx.Abort()
-	default:
-		/*
-		 * We don't care if the error occured is a networking error, faulty
-		 * logic or something else - from the user side this is an
-		 * InternalServerError regardless. At some point in the future, we might
-		 * want to deal with particular errors here.
-		 */
-		ctx.AbortWithStatus(http.StatusInternalServerError)
-	}
-}
-
-func parseManifest(doc []byte) (*message.Manifest, error) {
-	m := message.Manifest{}
-	return m.Unpack(doc)
-}
-
 func contains(haystack []int, needle int) bool {
 	for _, x := range haystack {
 		if x == needle {
@@ -222,13 +138,13 @@ func (s *Slice) Entry(ctx *gin.Context) {
 		ctx.AbortWithStatus(http.StatusInternalServerError)
 		return
 	}
-	manifest, err := getManifest(ctx, token, container)
+	manifest, err := util.GetManifest(ctx, token, container)
 	if err != nil {
 		log.Printf("%s %v", pid, err)
-		abortOnManifestError(ctx, err)
+		util.AbortOnManifestError(ctx, err)
 		return
 	}
-	m, err := parseManifest(manifest)
+	m, err := util.ParseManifest(manifest)
 	if err != nil {
 		log.Printf("%s %v", pid, err)
 		ctx.AbortWithStatus(http.StatusInternalServerError)
@@ -279,14 +195,14 @@ func (s *Slice) Get(ctx *gin.Context) {
 		ctx.AbortWithStatus(http.StatusInternalServerError)
 		return
 	}
-	manifest, err := getManifest(ctx, token, container)
+	manifest, err := util.GetManifest(ctx, token, container)
 	if err != nil {
 		log.Printf("%s %v", pid, err)
-		abortOnManifestError(ctx, err)
+		util.AbortOnManifestError(ctx, err)
 		return
 	}
 
-	m, err := parseManifest(manifest)
+	m, err := util.ParseManifest(manifest)
 	if err != nil {
 		log.Printf("%s %v", pid, err)
 		ctx.AbortWithStatus(http.StatusInternalServerError)
--- api/internal/util/util.go
+++ api/internal/util/util.go
@@ -1,8 +1,15 @@
 package util
 
 import (
-	"github.com/google/uuid"
+	"context"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+
+	"github.com/equinor/oneseismic/api/internal/message"
+	"github.com/Azure/azure-storage-blob-go/azblob"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 func MakePID() string {
@@ -11,4 +18,86 @@ func MakePID() string {
 
 func GeneratePID(ctx *gin.Context) {
 	ctx.Set("pid", MakePID())
+}
+
+/*
+ * Get the manifest for the cube from the blob store.
+ *
+ * It's important that this is a blocking read, since this is the first
+ * authorization mechanism in oneseismic. If the user (through the
+ * on-behalf-token) does not have permissions to read the manifest, it
+ * shouldn't be able to read the cube either. If so, no more processing should
+ * be done, and the request discarded.
+ */
+func GetManifest(
+	ctx context.Context,
+	token string,
+	containerURL *url.URL,
+) ([]byte, error) {
+	credentials := azblob.NewTokenCredential(token, nil)
+	pipeline    := azblob.NewPipeline(credentials, azblob.PipelineOptions{})
+	container   := azblob.NewContainerURL(*containerURL, pipeline)
+	blob        := container.NewBlobURL("manifest.json")
+
+	dl, err := blob.Download(
+		ctx,
+		0, /* offset */
+		azblob.CountToEnd,
+		azblob.BlobAccessConditions {},
+		false, /* content-get-md5 */
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	body := dl.Body(azblob.RetryReaderOptions{})
+	defer body.Close()
+	return ioutil.ReadAll(body)
+}
+
+/*
+ * Centralize the understanding of error conditions of azblob.download.
+ *
+ * There are two classes of errors:
+ * 1. A hard failure, i.e. the request itself failed, such as network
+ *    conditions.
+ * 2. A soft failure, i.e. the request itself suceeded, but was without
+ *    sufficient permissions, wrong syntax or similar. azblob probably handles
+ *    this by parsing the status code and maybe the response body.
+ *
+ * Most calls to GetManifest() should probably immediately call this function
+ * on error and exit, but it's moved into its own function so that error paths
+ * can be emulated and tested independently. An added benefit is that should a
+ * function, for some reason, need GetManifest() but want custom error+abort
+ * handling, it is sufficient to implement bespoke error handling and simply
+ * not call this.
+ */
+func AbortOnManifestError(ctx *gin.Context, err error) {
+	switch e := err.(type) {
+	case azblob.StorageError:
+		/*
+		 * Request successful, but the service returned some error e.g. a
+		 * non-existing cube, unauthorized request.
+		 *
+		 * For now, just write the status-text into the body, which should be
+		 * slightly more accurate than just the status code. Once the interface
+		 * matures, this should probably be a more structured error message.
+		 */
+		sc := e.Response()
+		ctx.String(sc.StatusCode, sc.Status)
+		ctx.Abort()
+	default:
+		/*
+		 * We don't care if the error occured is a networking error, faulty
+		 * logic or something else - from the user side this is an
+		 * InternalServerError regardless. At some point in the future, we might
+		 * want to deal with particular errors here.
+		 */
+		ctx.AbortWithStatus(http.StatusInternalServerError)
+	}
+}
+
+func ParseManifest(doc []byte) (*message.Manifest, error) {
+	m := message.Manifest{}
+	return m.Unpack(doc)
 }
