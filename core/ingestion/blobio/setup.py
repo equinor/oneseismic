37ead5f66ef1e99163857b7b4f34d699f7221381
Jørgen Kvalsvik <jokva@equinor.com>
2021-02-03 13:09:44 +0100

    Remove dead core/python library
    This library has been dead for a while (it never really came online),
    but carries some weight. Remove it outright, and if oneseismic should
    pull in a direction where it becomes useful again it is probably a
    better idea to start fresh than it is to twist this code into something
    else.

--- core/ingestion/blobio/setup.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name="blobio",
-    description="simple streams for azure blob store",
-    packages=["blobio"],
-    platforms="any",
-    install_requires=["azure-storage-blob"],
-)
