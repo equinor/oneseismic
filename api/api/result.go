c4df00a1135901820055506f10c798d1855ca7d9
Jørgen Kvalsvik <jokva@equinor.com>
2021-02-04 17:14:57 +0100

    Remove dead code from /result
    The /result endpoint has seen a lot of development, and a lot of cruft
    has been sitting around for a while. Remove the unused functions and
    their corresponding tests.
    
    While the file is much smaller now, it became pretty clear that the
    code supporting the /result endpoint is not really covered by tests.
    There are many reasons (some good) for this, but it makes it stand out
    as an opportunity for some improvement.

--- api/api/result.go
+++ api/api/result.go
@@ -5,14 +5,12 @@ import (
 	"encoding/binary"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/equinor/oneseismic/api/internal/auth"
 	"github.com/go-redis/redis/v8"
-	"github.com/Azure/azure-storage-blob-go/azblob"
 	"github.com/gin-gonic/gin"
 )
 
@@ -21,155 +19,6 @@ type Result struct {
 	StorageURL string
 	Storage    redis.Cmdable
 	Keyring    *auth.Keyring
-}
-
-/*
- * A super thin interface around the azblob download features, so that the
- * implementation can be substituted for testing
- */
-type dlerror struct {
-	status int
-	what   string
-}
-
-func (e *dlerror) Error() string {
-	return e.what
-}
-
-func downloaderror(status int, msg string) *dlerror {
-	return &dlerror {
-		status: status,
-		what: msg,
-	}
-}
-type storage interface {
-	download(ctx context.Context, id string) ([]byte, *dlerror)
-}
-
-type azstorage struct {
-	container azblob.ContainerURL
-}
-
-/*
- * This function exists to:
- * 1. Something callable to tests to ensure that responses are routed to the
- *    right channels
- * 2. Provide something that's easier to use both as a direct function call and
- *    a goroutine
- *
- * It is pretty trivial in its current form, but it's not impossible that
- * future additions and refactorings creates a need for more code than
- * container.download() and sending the result to the right channel.
- *
- * By writing it this way, the container.download can by synchronous, simple,
- * and with return values as you'd expect, and this function provides the extra
- * scaffolding needed to run it concurrently with the other download jobs.
- */
-func downloadToChannel(
-	ctx       context.Context,
-	cancel    context.CancelFunc,
-	container storage,
-	url       string,
-	results   chan []byte,
-	failures  chan *dlerror,
-) {
-	body, err := container.download(ctx, url)
-	if err != nil {
-		/*
-		 * send the cancel signal *before* posting on the failure channel, in
-		 * case the failure channel is blocked or not scheduled fast enough
-		 */
-		cancel()
-		failures <- err
-	} else {
-		results <- body
-	}
-}
-
-func collect(
-	parts   int,
-	success chan []byte,
-	failure chan *dlerror,
-	timeout time.Duration,
-) ([]byte, *dlerror) {
-	timer := time.NewTimer(timeout)
-	defer timer.Stop()
-
-	result := make([]byte, 5)
-	/* msgpack array type */
-	result[0] = 0xDD
-
-	/* msgpack array length, a 4-byte big-endian integer */
-	binary.BigEndian.PutUint32(result[1:], uint32(parts))
-	for i := 0; i < parts; i++ {
-		select {
-		case fail := <-failure:
-			return nil, fail
-
-			/*
-			 * TODO: this result can probably be streamed directly to client,
-			 * rather than being buffered up with append. However, doing so is
-			 * awkward because Content-Length won't be set, and handling errors
-			 * probably becomes more difficult
-			 */
-		case part := <-success:
-			result = append(result, part...)
-
-		case <- timer.C:
-			msg := fmt.Sprintf("timed out; got %d parts", i)
-			return nil, downloaderror(http.StatusInternalServerError, msg)
-		}
-	}
-	return result, nil
-}
-
-func (az *azstorage) download(
-	ctx context.Context,
-	id string,
-) ([]byte, *dlerror) {
-	url := az.container.NewBlobURL(id)
-	dl, err := url.Download(
-		ctx,
-		0, /* offset */
-		azblob.CountToEnd,
-		azblob.BlobAccessConditions {},
-		false, /* content-get-md5 */
-		azblob.ClientProvidedKeyOptions {},
-	)
-
-	/*
-	 * TODO: query properly what's going on here.
-	 * - Did the download fail?
-	 * - Not authorized?
-	 * - 404/not found?
-	 * - Something misconfigured?
-	 *
-	 * 404 probably means that the object hasn't been written _yet_, in the
-	 * case of data. If the header has already been read, the result is either
-	 * underway, or has failed. Right now there's no way to tell, but since
-	 * this function is used both for downloading the header and result
-	 * payload, retry logic should probably be wherever this is called.
-	 *
-	 * Eventually, this should always fail hard on 404, and just make clients
-	 * re-do the request at a later time.
-	 */
-	if err != nil {
-		return nil, downloaderror(http.StatusInternalServerError, err.Error())
-	}
-
-	body := dl.Body(azblob.RetryReaderOptions{})
-	defer body.Close()
-	bytes, err := ioutil.ReadAll(body)
-	/*
-	 * An error here probably only means broken connection midway through
-	 * transfer, since the Download() call succeeded. There's probably no
-	 * recovery from this, so just return the error as-is and let the request
-	 * be aborted
-	 */
-	if err != nil {
-		return nil, downloaderror(http.StatusInternalServerError, err.Error())
-	}
-	return bytes, nil
 }
 
 /*
