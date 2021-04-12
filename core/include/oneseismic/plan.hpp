6e406577b2f8af8d1c2dad845d9714f1935e21e8
Jørgen Kvalsvik <jokva@equinor.com>
2021-04-12 14:17:37 +0200

    Add not_found exception type
    This exception is an out-of-range error for use input, which generally
    should map directly to 404.

--- core/include/oneseismic/plan.hpp
+++ core/include/oneseismic/plan.hpp
@@ -1,10 +1,15 @@
 #ifndef ONESEISMIC_PLAN_HPP
 #define ONESEISMIC_PLAN_HPP
 
+#include <exception>
 #include <string>
 #include <vector>
 
 namespace one {
+
+struct not_found : public std::out_of_range {
+    using std::out_of_range::out_of_range;
+};
 
 std::vector< std::string >
 mkschedule(const char* doc, int len, int task_size) noexcept (false);
