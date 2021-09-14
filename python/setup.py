7f1f88ed9087964bc9dfd14dddabec6877d07485
Jørgen Kvalsvik <jokva@equinor.com>
2021-09-14 15:44:30 +0200

    Add decoding, simple submodules to packages

--- python/setup.py
+++ python/setup.py
@@ -8,6 +8,8 @@ if __name__ == "__main__":
             'oneseismic',
             'oneseismic.client',
             'oneseismic.internal',
+            'oneseismic.simple',
+            'oneseismic.decoding',
             'oneseismic.scan',
             'oneseismic.upload',
         ],
