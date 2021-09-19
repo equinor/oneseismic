867450ee29ccc21d116ba4d1428181b46fccf0db
Jørgen Kvalsvik <jokva@equinor.com>
2021-09-19 21:38:36 +0200

    Add test for cube.Linenumbers
    Add a test for the cube.Linenumbers resolver. The test is subtly
    valuable, as the communication between go and C++ can be a bit wonky,
    and is subject to change.
    
    The resolver is constructed directly, in order to not depend on going
    through some I/O step where the manifest is fetched from the blob store
    or a disk or memory device. This makes the test fast and reliable, at
    the cost of having to manually setup the cube+context internals.

---
+++ api/api/graphql_test.go
@@ -0,0 +1,64 @@
+package api
+
+import (
+	"context"
+	"reflect"
+	"testing"
+)
+
+func setupSession(t *testing.T, doc string) *QuerySession {
+	session := NewQuerySession()
+	err := session.InitWithManifest([]byte(doc))
+	if err != nil {
+		t.Logf("Could not init session")
+		t.Logf("Has manifest definition changed?")
+		t.Fatalf("%v", err)
+	}
+	return session
+}
+
+/*
+ * This test is subtly more valuable than it lets on.
+ *
+ * There is a chance that the communication between C++ and go is brittle,
+ * sending json.RawString (alias for []byte) about and praying that it decodes
+ * into the right thing.
+ */
+func TestLinenumbersReturnsExpected(t *testing.T) {
+	doc := `{
+		"format-version": 1,
+		"guid": "<some-id>",
+		"data": [{
+				"file-extension": "f32",
+				"filters": [],
+				"shapes": [[3, 3, 3]],
+				"prefix": "src",
+				"resolution": "source"
+			}],
+		"attributes": [],
+		"line-numbers": [
+				[9961, 9963, 9965],
+				[1961, 1962, 1963],
+				[0, 4000, 8000]
+			],
+		"line-labels": ["inline", "crossline", "time"]
+	} `
+	qctx := queryContext {
+		session: setupSession(t, doc),
+	}
+	ctx := setQueryContext(context.Background(), &qctx)
+	c := cube {}
+	numbers, err := c.Linenumbers(ctx)
+
+	expected := [][]int32{
+		{9961, 9963, 9965},
+		{1961, 1962, 1963},
+		{   0, 4000, 8000},
+	}
+	if err != nil {
+		t.Errorf("expected success; got %v", err)
+	}
+	if !reflect.DeepEqual(numbers, expected) {
+		t.Errorf("expected %v; got %v", expected, numbers)
+	}
+}
