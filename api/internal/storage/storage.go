bcb196b74d1d4f9eb03828c46346adc2bad8e648
Erlend Hårstad <erha@equinor.com>
2022-04-08 10:18:47 +0200

    Introduce a storage facade
    This commit introduces a storage facade in form of a go interface which
    aims to hide the details of the underlying storage (e.g. Azure
    BlobStore) from the rest of the system.
    
    The storage facade achieves three things:
    - The interaction with storage backend(s) is centralized in a single place.
    - It makes testing easier through custom storage implementations.
    - Implementations can and should support optional caching.

---
+++ api/internal/storage/storage.go
@@ -0,0 +1,24 @@
+package storage
+
+import (
+	"context"
+)
+
+
+type entity struct {
+	Data []byte
+	Tag  *string
+}
+
+/*
+ *  Minimal interface for fetching blobs/files from storage. This hides a lot of
+ *  feature and details about the underlying storage from the rest of the
+ *  system, making it easy to swap out the storage provider. This means testing
+ *  becomes easier through custom storage implementations.
+ */
+type StorageClient interface {
+	/*
+	 * Get a blob or file from storage
+	 */
+	Get(ctx context.Context, blob string) (*entity, error)
+}
