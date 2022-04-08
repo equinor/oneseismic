157b895a87bcc52034e7cce05f4d30228699f49e
Erlend Hårstad <erha@equinor.com>
2022-04-08 15:44:09 +0200

    Remove cache implementations from pipeline.go
    The caching has already been moved to the storage package [1], and now
    that fetch/pipeline is ported to using that [2] it's time to complete
    the move by removing it from pipeline.
    
    [1] SHA: 93475c7901caa7325ffdd88a2c2b1868fed6c7dc
    [2] SHA: 25344a8c621cf7decb8e8ab495b325e9fda14dec

--- api/cmd/fetch/pipeline.go
+++ api/cmd/fetch/pipeline.go
@@ -5,62 +5,7 @@ import (
 	"net/url"
 
 	"github.com/equinor/oneseismic/api/internal/storage"
-
-	"github.com/dgraph-io/ristretto"
 )
-
-/*
- * This module implements a worker pool for blob fetches, with caching. The
- * intended use is for the main function to spawn a worker group that pulls
- * URLs and serves downloaded, possibly cached, blobs. The worker pool are
- * stupid pipes and have no context or reference to the task that provided the
- * URLs.
- */
-
-/*
- * Silly & minimal interface to a cache of fragments - this hides a lot of
- * features, but should make some testing and benchmarking easier by providing
- * a way to tune or disable the cache.
- *
- * It achieves two things:
- * 1. ease-of-testing through custom cache implementations
- * 2. automates the casting, forcing the cache to only store the cacheentry
- *    type, which is way less annoying than dealing with interface{}
- */
-type fragmentcache interface {
-	set(string, cacheEntry)
-	get(string) (cacheEntry, bool)
-}
-
-type cacheEntry struct {
-	chunk []byte
-	etag  *string
-}
-
-type ristrettocache struct {
-	ristretto.Cache
-}
-func (c *ristrettocache) set(key string, val cacheEntry) {
-	c.Set(key, val, 0)
-}
-func (c *ristrettocache) get(key string) (val cacheEntry, hit bool) {
-	v, hit := c.Get(key)
-	if hit {
-		val = v.(cacheEntry)
-	}
-	return
-}
-
-/*
- * The nocache isn't really used per now, but serves as a useful reference and
- * available information for tests runs or test cases that wants to disable
- * caching altogether.
- */
-type nocache struct {}
-func (c *nocache) set(key string, val cacheEntry) {}
-func (c *nocache) get(key string) (cacheEntry, bool) {
-	return cacheEntry{}, false
-}
 
 /*
  * The downloaded fragment (as it is stored in blob). The index is a key, used
